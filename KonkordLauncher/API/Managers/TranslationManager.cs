0c8488ef1217692cf863e9e6b92c3317d21ec402
Tavstal <tavstal@gmail.com>
2024-03-14 23:35:09 +0100

    Added a WIP TranslationManager

---
+++ KonkordLauncher/API/Managers/TranslationManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonkordLauncher.API.Managers
+{
+    public static class TranslationManager
+    {
+        private static Dictionary<string, string> _translations = new Dictionary<string ,string>();
+        private static bool _initialized = false;
+
+        public static Dictionary<string, string> Translations { get { return _translations; } }
+
+        public static void Init(string language)
+        {
+            if (_initialized)
+                return;
+
+            
+
+            _initialized = true;
+        }
+
+        public static string Translate(string key, params object[]? args)
+        {
+            string result = string.Empty;
+
+            if (Translations.ContainsKey(key))
+            {
+                result = string.Format(Translations[key], args);
+            }
+            else
+            {
+                throw new NullReferenceException($"The translations does not contain the '{key}' key.");
+            }
+
+            return result;
+        }
+
+    }
+}

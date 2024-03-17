6fbb0bf9be7480e46fb168f9ba4a8475dc2fcbb0
Tavstal <tavstal@gmail.com>
2024-03-17 21:37:30 +0100

    Added NotificationHelper

---
+++ KonkordLauncher/API/Helpers/NotificationHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace KonkordLauncher.API.Helpers
+{
+    public static class NotificationHelper
+    {
+
+        public static void SendNotification(string message, string title, MessageBoxImage image = MessageBoxImage.Information)
+        {
+            MessageBox.Show(message, title, MessageBoxButton.OK, image);
+        }
+
+        public static void SendInfo(string message, string title)
+        {
+            SendNotification(message, title);
+        }
+
+        public static void SendWarning(string message, string title)
+        {
+            SendNotification(message, title, MessageBoxImage.Warning);
+        }
+
+        public static void SendError(string message, string title)
+        {
+            SendNotification(message, title, MessageBoxImage.Error);
+        }
+    }
+}

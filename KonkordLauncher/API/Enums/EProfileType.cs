142fb8fbb9f4c8e5181bfa2ac1a853c43e6becfe
Tavstal <tavstal@gmail.com>
2024-03-17 21:36:23 +0100

    Added EProfileType

---
+++ KonkordLauncher/API/Enums/EProfileType.cs
@@ -0,0 +1,11 @@
+﻿namespace KonkordLauncher.API.Enums
+{
+    public enum EProfileType
+    {
+        LATEST_RELEASE = 0,
+        LATEST_SNAPSHOT = 1,
+        KONKORD_CREATE = 2,
+        KONKORD_VANILLAPLUS = 3,
+        CUSTOM = 4
+    }
+}

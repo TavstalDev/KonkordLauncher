652f1cbfb16e60f57f0db3710d49d3846e1efba0
Tavstal <tavstal@gmail.com>
2024-03-17 21:36:05 +0100

    Added ELauncherVisibility

---
+++ KonkordLauncher/API/Enums/ELaucnherVisibility.cs
@@ -0,0 +1,9 @@
+﻿namespace KonkordLauncher.API.Enums
+{
+    public enum ELaucnherVisibility
+    {
+        HIDE_AND_REOPEN_ON_GAME_CLOSE = 0,
+        CLOSE_ON_GAME_START = 1,
+        KEEP_OPEN = 2
+    }
+}

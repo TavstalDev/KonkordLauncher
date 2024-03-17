5eaa785b9ea352b999ae7ba9134aa89b3af312b0
Tavstal <tavstal@gmail.com>
2024-03-17 21:38:14 +0100

    Added a basic TranslationManager, wip

--- KonkordLauncher/API/Managers/TranslationManager.cs
+++ KonkordLauncher/API/Managers/TranslationManager.cs
@@ -1,26 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using KonkordLauncher.API.Helpers;
 
 namespace KonkordLauncher.API.Managers
 {
     public static class TranslationManager
     {
-        private static Dictionary<string, string> _translations = new Dictionary<string ,string>();
+        private static Dictionary<string, string> _translations = new Dictionary<string, string>();
         private static bool _initialized = false;
 
         public static Dictionary<string, string> Translations { get { return _translations; } }
 
-        public static void Init(string language)
+        private static readonly Dictionary<string, string> _defaultTranslations = new Dictionary<string, string>()
+        {
+            { "", "" },
+        };
+
+        public static Dictionary<string, string> DefaultTranslations { get { return _defaultTranslations; } }
+
+        private static Dictionary<string, string> _languagePacks = new Dictionary<string, string>()
+        {
+
+        };
+        public static Dictionary<string, string> LanguagePacks { get { return _languagePacks; } }
+
+        public static void SetTranslations(Dictionary<string, string>? translation)
         {
             if (_initialized)
                 return;
 
-            
+            if (translation == null)
+                _translations = _defaultTranslations;
+            else
+                _translations = translation;
 
             _initialized = true;
+        }
+
+        public static async Task<Dictionary<string, string>?> ReadTranslationAsync(string path)
+        {
+            try
+            {
+                return await JsonHelper.ReadJsonFile<Dictionary<string, string>>(path);
+            }
+            catch (Exception ex)
+            {
+                NotificationHelper.SendError(ex.ToString(), "Error in SaveTranslations");
+                return null;
+            }
+        }
+
+        public static async Task<bool> SaveTranslationAsync(string path, Dictionary<string, string> translation)
+        {
+            try
+            {
+                return await JsonHelper.WriteJsonFile(path, translation);
+            }
+            catch (Exception ex)
+            {
+                NotificationHelper.SendError(ex.ToString(), "Error in SaveTranslations");
+                return false;
+            }
         }
 
         public static string Translate(string key, params object[]? args)
@@ -33,7 +71,8 @@ namespace KonkordLauncher.API.Managers
             }
             else
             {
-                throw new NullReferenceException($"The translations does not contain the '{key}' key.");
+                NotificationHelper.SendError($"The translations does not contain the '{key}' key.", "Error in SaveTranslations");
+                return string.Empty;
             }
 
             return result;

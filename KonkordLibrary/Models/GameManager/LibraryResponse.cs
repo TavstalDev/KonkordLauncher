4f5147e58198c1025d5d517f435d36082da03035
Tavstal <tavstal@gmail.com>
2024-03-22 23:23:46 +0100

    Added custom arg fields

--- KonkordLibrary/Models/GameManager/LibraryResponse.cs
+++ KonkordLibrary/Models/GameManager/LibraryResponse.cs
@@ -11,6 +11,8 @@ namespace KonkordLibrary.Models.GameManager
         public string LibrarySizeCachePath { get; set; }
         public int LocalLibrarySize { get; set; }
         public JArray Libraries {  get; set; }
+        public List<string> CustomGameArgs { get; set; }
+        public List<string> CustomJavaArgs { get; set; }
 
         public LibraryResponse() { }
 

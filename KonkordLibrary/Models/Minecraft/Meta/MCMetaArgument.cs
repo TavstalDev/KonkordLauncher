9d2ee175bfc40af04bf19f59fe213020013e4ac3
Tavstal <tavstal@gmail.com>
2024-03-26 20:40:43 +0100

    Added functions to get arguments. (temporal solution)

--- KonkordLibrary/Models/Minecraft/Meta/MCMetaArgument.cs
+++ KonkordLibrary/Models/Minecraft/Meta/MCMetaArgument.cs
@@ -17,5 +17,78 @@ namespace KonkordLibrary.Models.Minecraft.Meta
             Game = game;
             JVM = jVM;
         }
+
+        public List<string> GetGameArgs()
+        {
+            List<string> local = new List<string>();
+            foreach (var item in Game)
+            {
+                if (item is string s)
+                    local.Add(s);
+            }
+
+            List<string> result = new List<string>();
+
+            for (int i = 0; i < local.Count; i += 2)
+            {
+                result.Add(local[i] + " " + local[i + 1]);
+            }
+
+            return result;
+        }
+
+        public string GetGameArgString()
+        {
+            List<string> local = new List<string>();
+            foreach (var item in Game)
+            {
+                if (item is string s)
+                    local.Add(s);
+            }
+
+            string result = string.Empty;
+            for (int i = 0; i < local.Count; i += 2)
+            {
+                result += $"{local[i]} {local[i + 1]} ";
+            }
+
+            return result;
+        }
+
+        public List<string> GetJVMArgs()
+        {
+            List<string> local = new List<string>();
+            foreach (var item in JVM)
+            {
+                if (item is string s)
+                {
+                    if (s.StartsWith("-cp"))
+                        continue;
+                    if (s == "${classpath}")
+                        continue;
+                    local.Add(s);
+                }
+            }
+
+            return local;
+        }
+
+        public string GetJVMArgString()
+        {
+            string local = string.Empty;
+            foreach (var item in JVM)
+            {
+                if (item is string s)
+                {
+                    if (s.StartsWith("-cp"))
+                        continue;
+                    if (s == "${classpath}")
+                        continue;
+                    local += $"{s} ";
+                }
+            }
+
+            return local;
+        }
     }
 }

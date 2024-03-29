1803d3e8d926ba69634c8635e42f8ddbe3f45a9f
Tavstal <tavstal@gmail.com>
2024-03-29 10:21:23 +0100

    Added directory related functions

--- KonkordLibrary/Helpers/IOHelper.cs
+++ KonkordLibrary/Helpers/IOHelper.cs
@@ -397,6 +397,52 @@ namespace KonkordLibrary.Helpers
         #endregion
 
         #region Functions
+        public static void DeleteDirectory(string path)
+        {
+            var forgeInstallerDirInfo = new DirectoryInfo(path);
+            foreach (FileInfo file in forgeInstallerDirInfo.GetFiles())
+                file.Delete();
+            foreach (DirectoryInfo subDirectory in forgeInstallerDirInfo.GetDirectories())
+                subDirectory.Delete(true);
+            Directory.Delete(path);
+        }
+
+        public static void MoveDirectory(string sourceDir, string destinationDir, bool recursive, bool deleteSource = true)
+        {
+            // Get information about the source directory
+            var dir = new DirectoryInfo(sourceDir);
+
+            // Check if the source directory exists
+            if (!dir.Exists)
+                throw new DirectoryNotFoundException($"Source directory not found: {dir.FullName}");
+
+            // Cache directories before we start copying
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            // Create the destination directory
+            Directory.CreateDirectory(destinationDir);
+
+            // Get the files in the source directory and copy to the destination directory
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                string targetFilePath = Path.Combine(destinationDir, file.Name);
+                file.CopyTo(targetFilePath, true);
+            }
+
+            // If recursive and copying subdirectories, recursively call this method
+            if (recursive)
+            {
+                foreach (DirectoryInfo subDir in dirs)
+                {
+                    string newDestinationDir = Path.Combine(destinationDir, subDir.Name);
+                    MoveDirectory(subDir.FullName, newDestinationDir, true, false);
+                }
+            }
+
+            if (deleteSource)
+                DeleteDirectory(sourceDir);
+        }
+
         /// <summary>
         /// Retrieves the launcher settings, if available.
         /// </summary>

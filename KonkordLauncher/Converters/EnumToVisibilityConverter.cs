3a11c8ca64f77140840f106a2771ad67f378c1d8
Tavstal <tavstal@gmail.com>
2024-03-31 08:35:46 +0200

    Added EnumToVisibilityConverter

---
+++ KonkordLauncher/Converters/EnumToVisibilityConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace KonkordLauncher.Converters
+{
+    [ValueConversion(typeof(Enum), typeof(bool))]
+    public class EnumToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || !(value is Enum))
+                return Visibility.Collapsed;
+
+            Enum enumValue = (Enum)value;
+
+            // Modify this logic based on your requirements
+            return enumValue.Equals(parameter) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}

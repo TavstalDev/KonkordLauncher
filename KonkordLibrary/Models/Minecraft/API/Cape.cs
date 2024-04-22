52662890f6ccd91315073f3c72690d2802c3fb89
Tavstal <tavstal@gmail.com>
2024-04-22 12:36:22 +0200

    Updated Cape model

--- KonkordLibrary/Models/Minecraft/API/Cape.cs
+++ KonkordLibrary/Models/Minecraft/API/Cape.cs
@@ -1,6 +1,27 @@
-﻿namespace Tavstal.KonkordLibrary.Models.Minecraft.API
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace Tavstal.KonkordLibrary.Models.Minecraft.API
 {
     public class Cape
     {
+        [JsonPropertyName("id"), JsonProperty("id")]
+        public string Id {  get; set; }
+        [JsonPropertyName("state"), JsonProperty("state")]
+        public string State {  get; set; }
+        [JsonPropertyName("url"), JsonProperty("url")]
+        public string Url {  get; set; }
+        [JsonPropertyName("alias"), JsonProperty("alias")]
+        public string Alias {  get; set; }
+
+        public Cape() { }
+
+        public Cape(string id, string state, string url, string alias)
+        {
+            Id = id;
+            State = state;
+            Url = url;
+            Alias = alias;
+        }
     }
 }

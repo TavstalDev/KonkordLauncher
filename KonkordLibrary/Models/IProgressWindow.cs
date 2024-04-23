b9b31d53423f66a531f16c6cc0c1eb803c7d9eea
Tavstal <tavstal@gmail.com>
2024-04-23 13:25:50 +0200

    Added interface for ProgressWindow

---
+++ KonkordLibrary/Models/IProgressWindow.cs
@@ -0,0 +1,9 @@
+﻿namespace KonkordLibrary.Models
+{
+    public interface IProgressWindow
+    {
+        void UpdateProgressBar(double percent, string text);
+
+        void UpdateProgressBarTranslated(double percent, string text, params object[]? args);
+    }
+}

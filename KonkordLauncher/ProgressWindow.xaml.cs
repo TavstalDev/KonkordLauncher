41155d710759063a6b113e83bb59739f913cc4cd
Tavstal <tavstal@gmail.com>
2024-04-23 13:26:07 +0200

    Finished progress window

--- KonkordLauncher/ProgressWindow.xaml.cs
+++ KonkordLauncher/ProgressWindow.xaml.cs
@@ -1,27 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using KonkordLibrary.Models;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using Tavstal.KonkordLibrary.Helpers;
+using Tavstal.KonkordLibrary.Managers;
 
 namespace KonkordLauncher
 {
     /// <summary>
     /// Interaction logic for ProgressWindow.xaml
     /// </summary>
-    public partial class ProgressWindow : Window
+    public partial class ProgressWindow : Window, IProgressWindow
     {
         public ProgressWindow()
         {
             InitializeComponent();
+        }
+
+        public void UpdateProgressBar(double percent, string text)
+        {
+            pb_status.Value = percent > pb_status.Maximum ? pb_status.Maximum : percent;
+            lab_status.Content = text;
+        }
+
+        public void UpdateProgressBarTranslated(double percent, string text, params object[]? args)
+        {
+            pb_status.Value = percent > pb_status.Maximum ? pb_status.Maximum : percent;
+            lab_status.Content = TranslationManager.Translate(text, args);
+        }
+
+        /// <summary>
+        /// Event handler for minimizing the window when a specific button is clicked.
+        /// </summary>
+        /// <param name="sender">The object that raised the event.</param>
+        /// <param name="e">The event arguments.</param>
+        private void WindowMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
         }
     }
 }

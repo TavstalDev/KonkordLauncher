8600f4082346dc97cca61dd12b189e31cababefb
tavstal <tavstal@gmail.com>
2025-07-17 14:46:22 +0200

    Add initial Avalonia application structure for Desktop project

---
+++ Desktop/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Tavstal.KonkordLauncher.Desktop;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}

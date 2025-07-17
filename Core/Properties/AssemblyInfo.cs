5467d8aa2fdf2196c18b689133451bd251cdc743
tavstal <tavstal@gmail.com>
2025-07-17 14:47:14 +0200

    Rename AssemblyInfo for Core project and update metadata

---
+++ Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Core")]
+[assembly: AssemblyDescription("Core used for KonkordLauncher.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Tavstal")]
+[assembly: AssemblyProduct("Core")]
+[assembly: AssemblyCopyright("Copyright © 2025 Tavstal")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyVersion("1.0.*")]

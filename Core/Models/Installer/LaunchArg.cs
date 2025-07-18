f645187d123e542c4298548980b2e47de584ec94
tavstal <tavstal@gmail.com>
2025-07-19 00:49:16 +0200

    Enhance LaunchArg and ModdedData classes with XML documentation and rename ModedData to ModdedData

--- Core/Models/Installer/LaunchArg.cs
+++ Core/Models/Installer/LaunchArg.cs
@@ -1,16 +1,33 @@
-﻿namespace Tavstal.KonkordLauncher.Core.Models.Installer
+﻿namespace Tavstal.KonkordLauncher.Core.Models.Installer;
+
+/// <summary>
+/// Represents a launch argument with a specified priority.
+/// </summary>
+public class LaunchArg
 {
-    public class LaunchArg
+    /// <summary>
+    /// Gets or sets the launch argument as a string.
+    /// </summary>
+    public string Arg { get; set; }
+
+    /// <summary>
+    /// Gets or sets the priority of the launch argument.
+    /// </summary>
+    public int Priority { get; set; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="LaunchArg"/> class.
+    /// </summary>
+    public LaunchArg() { }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="LaunchArg"/> class with a specified argument and priority.
+    /// </summary>
+    /// <param name="arg">The launch argument as a string.</param>
+    /// <param name="priority">The priority of the launch argument.</param>
+    public LaunchArg(string arg, int priority)
     {
-        public string Arg {  get; set; }
-        public int Priority { get; set; }
-
-        public LaunchArg() { }
-
-        public LaunchArg(string arg, int priority)
-        {
-            Arg = arg;
-            Priority = priority;
-        }
+        Arg = arg;
+        Priority = priority;
     }
 }
---
+++ Core/Models/Installer/ModdedData.cs
@@ -0,0 +1,43 @@
+﻿using Tavstal.KonkordLauncher.Core.Models.Launcher;
+using Tavstal.KonkordLauncher.Core.Models.MojangApi.Meta;
+
+namespace Tavstal.KonkordLauncher.Core.Models.Installer;
+
+/// <summary>
+/// Represents the modded data required for the installation process.
+/// </summary>
+public class ModdedData
+{
+    /// <summary>
+    /// Gets or sets the main class of the modded data.
+    /// </summary>
+    public string MainClass { get; set; }
+
+    /// <summary>
+    /// Gets or sets the version details associated with the modded data.
+    /// </summary>
+    public VersionDetails VersionData { get; set; }
+
+    /// <summary>
+    /// Gets or sets the list of libraries required for the modded data.
+    /// </summary>
+    public List<LibraryMeta> Libraries { get; set; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ModdedData"/> class.
+    /// </summary>
+    public ModdedData() { }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ModdedData"/> class with specified main class, version details, and libraries.
+    /// </summary>
+    /// <param name="mainClass">The main class of the modded data.</param>
+    /// <param name="versionData">The version details associated with the modded data.</param>
+    /// <param name="libraries">The list of libraries required for the modded data.</param>
+    public ModdedData(string mainClass, VersionDetails versionData, List<LibraryMeta> libraries)
+    {
+        MainClass = mainClass;
+        VersionData = versionData;
+        Libraries = libraries;
+    }
+}

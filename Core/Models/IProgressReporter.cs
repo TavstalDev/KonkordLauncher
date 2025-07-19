d8ca79b1acb72b4d1c828592f3227bd3d0ee6663
tavstal <tavstal@gmail.com>
2025-07-19 16:00:11 +0200

    Add SetStatusTranslated method to IProgressReporter for localized status updates

--- Core/Models/IProgressReporter.cs
+++ Core/Models/IProgressReporter.cs
@@ -5,6 +5,8 @@ public interface IProgressReporter
     void SetProgress(double progress);
     
     void SetStatus(string status);
+    
+    void SetStatusTranslated(string statusKey, params object[]? args);
 
     void Show();
     

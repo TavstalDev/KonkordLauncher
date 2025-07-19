6d6282c3e4fe776d4283c3690d7906e576a7c7a5
tavstal <tavstal@gmail.com>
2025-07-19 16:01:10 +0200

    Add EMinecraftKind enum to represent different Minecraft profile types

---
+++ Core/Enums/EMinecraftKind.cs
@@ -0,0 +1,28 @@
+﻿namespace Tavstal.KonkordLauncher.Core.Enums
+{
+    /// <summary>
+    /// Represents the different kinds of Minecraft profiles supported by the launcher.
+    /// </summary>
+    public enum EMinecraftKind
+    {
+        /// <summary>
+        /// Represents the Vanilla version of Minecraft.
+        /// </summary>
+        VANILLA = 0,
+
+        /// <summary>
+        /// Represents the Forge modded version of Minecraft.
+        /// </summary>
+        FORGE = 1,
+
+        /// <summary>
+        /// Represents the Fabric modded version of Minecraft.
+        /// </summary>
+        FABRIC = 2,
+
+        /// <summary>
+        /// Represents the Quilt modded version of Minecraft.
+        /// </summary>
+        QUILT = 3
+    }
+}

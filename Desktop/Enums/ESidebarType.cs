b07ed06cf0736c061746c541d5e66b4f39350c1f
tavstal <tavstal@gmail.com>
2025-07-20 18:25:32 +0200

    Add ESidebarType enum to represent sidebar options in the application

---
+++ Desktop/Enums/ESidebarType.cs
@@ -0,0 +1,32 @@
+namespace Tavstal.KonkordLauncher.Desktop.Enums;
+
+/// <summary>
+/// Represents the different types of sidebar options available in the application.
+/// </summary>
+public enum ESidebarType
+{
+    /// <summary>
+    /// The "Play" sidebar option.
+    /// </summary>
+    Play = 0,
+
+    /// <summary>
+    /// The "Instances" sidebar option, used for managing game instances.
+    /// </summary>
+    Instances = 1,
+
+    /// <summary>
+    /// The "News" sidebar option, displaying news and updates.
+    /// </summary>
+    News = 2,
+
+    /// <summary>
+    /// The "Accounts" sidebar option, for managing user accounts.
+    /// </summary>
+    Accounts = 3,
+
+    /// <summary>
+    /// The "Settings" sidebar option, for configuring application settings.
+    /// </summary>
+    Settings = 4
+}

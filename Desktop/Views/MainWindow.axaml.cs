e9401fcda69d122f49d192720ca6ffc6bc09a584
tavstal <tavstal@gmail.com>
2025-07-21 16:37:55 +0200

    Added settings, merged play & instances section by refactoring play cards

--- Desktop/Views/MainWindow.axaml.cs
+++ Desktop/Views/MainWindow.axaml.cs
@@ -2,6 +2,7 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Tavstal.KonkordLauncher.Desktop.Enums;
+using Tavstal.KonkordLauncher.Desktop.Models;
 using Tavstal.KonkordLauncher.Desktop.ViewModels;
 
 namespace Tavstal.KonkordLauncher.Desktop.Views;
@@ -21,86 +22,97 @@ public partial class MainWindow : Window
         // Instantiate your ViewModel and assign it to the DataContext
         this.DataContext = new MainWindowViewModel();
         _selectedButton = PlaySideBtn;
+    }
+    
+    private void MainWindow_Loaded(object? sender, RoutedEventArgs e)
+    {
+        UpdateInstancesOnPlayPage();
+    }
+
+    #region Methods
+
+    private void HandleSidebarChange(ESidebarType sidebarType)
+    {
+        if (DataContext is not MainWindowViewModel viewModel)
+            return;
+        
+        if (viewModel.CurrentPageIndex == sidebarType)
+            return;
+
+        viewModel.CurrentPageIndex = sidebarType;
+        _selectedButton.Classes.Remove("SidebarSelectedBtn");
+        _selectedButton.Classes.Add("SidebarBtn");
+
+        switch (sidebarType)
+        {
+            case ESidebarType.Play:
+            {
+                PlaySideBtn.Classes.Remove("SidebarBtn");
+                PlaySideBtn.Classes.Add("SidebarSelectedBtn");
+                _selectedButton = PlaySideBtn;
+                break;
+            }
+            case ESidebarType.News:
+            {
+                NewsSideBtn.Classes.Remove("SidebarBtn");
+                NewsSideBtn.Classes.Add("SidebarSelectedBtn");
+                _selectedButton = NewsSideBtn;
+                break;
+            }
+            case ESidebarType.Accounts:
+            {
+                AccountsSideBtn.Classes.Remove("SidebarBtn");
+                AccountsSideBtn.Classes.Add("SidebarSelectedBtn");
+                _selectedButton = AccountsSideBtn;
+                break;
+            }
+            case ESidebarType.Settings:
+            {
+                SettingsSideBtn.Classes.Remove("SidebarBtn");
+                SettingsSideBtn.Classes.Add("SidebarSelectedBtn");
+                _selectedButton = SettingsSideBtn;
+                break;
+            }
+        }
 
     }
-    
+
+    private void UpdateInstancesOnPlayPage()
+    {
+        if (DataContext is not MainWindowViewModel viewModel)
+            return;
+
+        // TODO: Here you would typically fetch or update the instances on the play page.
+        // For demonstration, let's assume we are adding a new instance.
+        viewModel.InstancesOnPlayPage.Add(new PlayCardModel { Title = "New Instance" });
+        
+        // After Updating:
+        bool hasInstances = viewModel.InstancesOnPlayPage.Count > 0;
+        NoPlayInstancesTextBlock.IsVisible = !hasInstances;
+    }
+    #endregion
+
+    #region Event Handlers
+
     public void OnPlaySideButtonClick(object? sender, RoutedEventArgs e)
     {
-        if (DataContext is MainWindowViewModel viewModel)
-        {
-            if (viewModel.CurrentPageIndex == ESidebarType.Play)
-                return;
-            
-            viewModel.CurrentPageIndex = ESidebarType.Play;
-            _selectedButton.Classes.Remove("SidebarSelectedBtn");
-            _selectedButton.Classes.Add("SidebarBtn");
-            PlaySideBtn.Classes.Remove("SidebarBtn");
-            PlaySideBtn.Classes.Add("SidebarSelectedBtn");
-            _selectedButton = PlaySideBtn;
-        }
-    }
-    
-    public void OnInstancesSideButtonClick(object? sender, RoutedEventArgs e)
-    {
-        if (DataContext is MainWindowViewModel viewModel)
-        {
-            if (viewModel.CurrentPageIndex == ESidebarType.Instances)
-                return;
-            
-            viewModel.CurrentPageIndex = ESidebarType.Instances;
-            _selectedButton.Classes.Remove("SidebarSelectedBtn");
-            _selectedButton.Classes.Add("SidebarBtn");
-            InstancesSideBtn.Classes.Remove("SidebarBtn");
-            InstancesSideBtn.Classes.Add("SidebarSelectedBtn");
-            _selectedButton = InstancesSideBtn;
-        }
+        HandleSidebarChange(ESidebarType.Play);
     }
     
     public void OnNewsSideButtonClick(object? sender, RoutedEventArgs e)
     {
-        if (DataContext is MainWindowViewModel viewModel)
-        {
-            if (viewModel.CurrentPageIndex == ESidebarType.News)
-                return;
-            
-            viewModel.CurrentPageIndex = ESidebarType.News;
-            _selectedButton.Classes.Remove("SidebarSelectedBtn");
-            _selectedButton.Classes.Add("SidebarBtn");
-            NewsSideBtn.Classes.Remove("SidebarBtn");
-            NewsSideBtn.Classes.Add("SidebarSelectedBtn");
-            _selectedButton = NewsSideBtn;
-        }
+        HandleSidebarChange(ESidebarType.News);
     }
     
     public void OnAccountsSideButtonClick(object? sender, RoutedEventArgs e)
     {
-        if (DataContext is MainWindowViewModel viewModel)
-        {
-            if (viewModel.CurrentPageIndex == ESidebarType.Accounts)
-                return;
-            
-            viewModel.CurrentPageIndex = ESidebarType.Accounts;
-            _selectedButton.Classes.Remove("SidebarSelectedBtn");
-            _selectedButton.Classes.Add("SidebarBtn");
-            AccountsSideBtn.Classes.Remove("SidebarBtn");
-            AccountsSideBtn.Classes.Add("SidebarSelectedBtn");
-            _selectedButton = AccountsSideBtn;
-        }
+        HandleSidebarChange(ESidebarType.Accounts);
     }
     
     public void OnSettingsSideButtonClick(object? sender, RoutedEventArgs e)
     {
-        if (DataContext is MainWindowViewModel viewModel)
-        {
-            if (viewModel.CurrentPageIndex == ESidebarType.Settings)
-                return;
-            
-            viewModel.CurrentPageIndex = ESidebarType.Settings;
-            _selectedButton.Classes.Remove("SidebarSelectedBtn");
-            _selectedButton.Classes.Add("SidebarBtn");
-            SettingsSideBtn.Classes.Remove("SidebarBtn");
-            SettingsSideBtn.Classes.Add("SidebarSelectedBtn");
-            _selectedButton = SettingsSideBtn;
-        }
+        HandleSidebarChange(ESidebarType.Settings);
     }
+
+    #endregion
 }

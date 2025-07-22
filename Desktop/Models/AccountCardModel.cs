f7d27cb877581205a953e8bef00e4d890e58bcbd
tavstal <tavstal@gmail.com>
2025-07-22 13:46:45 +0200

    Add AccountCardModel and NewsCardModel classes for account and news representation

---
+++ Desktop/Models/AccountCardModel.cs
@@ -0,0 +1,8 @@
+namespace Tavstal.KonkordLauncher.Desktop.Models;
+
+public class AccountCardModel
+{
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public bool IsSelected { get; set; }
+}
---
+++ Desktop/Models/NewsCardModel.cs
@@ -0,0 +1,10 @@
+namespace Tavstal.KonkordLauncher.Desktop.Models;
+
+public class NewsCardModel
+{
+    public string Title { get; set; }
+    
+    public string Description { get; set; }
+    
+    public string Url { get; set; }
+}

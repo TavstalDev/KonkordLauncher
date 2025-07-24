5c9792135312e2d1249f6ed184607d4be4621ae5
tavstal <tavstal@gmail.com>
2025-07-24 12:06:48 +0200

    Add VersionModel class to represent application version details

---
+++ Desktop/Models/VersionModel.cs
@@ -0,0 +1,8 @@
+namespace Tavstal.KonkordLauncher.Desktop.Models;
+
+public class VersionModel
+{
+    public string Version { get; set; }
+    public string ReleaseDate { get; set; }
+    public string Type { get; set; }
+}

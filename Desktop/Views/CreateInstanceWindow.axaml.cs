162abcefe976e86af7a85efdbe1b435b76888690
tavstal <tavstal@gmail.com>
2025-07-24 12:08:04 +0200

    Add CreateInstanceWindow layout and data binding for custom instance creation

--- Desktop/Views/CreateInstanceWindow.axaml.cs
+++ Desktop/Views/CreateInstanceWindow.axaml.cs
@@ -1,5 +1,8 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Tavstal.KonkordLauncher.Desktop.Models;
+using Tavstal.KonkordLauncher.Desktop.ViewModels;
 
 namespace Tavstal.KonkordLauncher.Desktop.Views;
 
@@ -13,6 +16,15 @@ public partial class CreateInstanceWindow : Window
         // Attaches Avalonia Dev Tools for debugging purposes.
         this.AttachDevTools();
 #endif
+
+        this.DataContext = new CreateInstanceViewModel();
         
+    }
+
+    private void Control_OnLoaded(object? sender, RoutedEventArgs e)
+    {
+        if (DataContext is not CreateInstanceViewModel vm) return;
+        
+        // TODO
     }
 }

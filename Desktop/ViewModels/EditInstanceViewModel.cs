4f7be3be04153df73d2ff1ea99e93c4cf620916d
tavstal <tavstal@gmail.com>
2025-07-28 18:45:28 +0200

    Add EditInstanceViewModel to manage collections of mods, resource packs, shader packs, worlds, servers, and screenshots

---
+++ Desktop/ViewModels/EditInstanceViewModel.cs
@@ -0,0 +1,75 @@
+using System.Collections.ObjectModel;
+using Tavstal.KonkordLauncher.Desktop.Models;
+
+namespace Tavstal.KonkordLauncher.Desktop.ViewModels;
+
+public class EditInstanceViewModel : ViewModelBase
+{
+    public ObservableCollection<ModModel> Mods { get; set; }
+    
+    public ObservableCollection<ResourcePackModel> ResourcePacks { get; set; }
+    
+    public ObservableCollection<ShaderPackModel> ShaderPacks { get; set; }
+    
+    public ObservableCollection<WorldModel> Worlds { get; set; }
+    
+    public ObservableCollection<ServerModel> Servers { get; set; }
+    
+    public ObservableCollection<ScreenshotModel> Screenshots { get; set; }
+    
+    public EditInstanceViewModel()
+    {
+        Mods =
+        [
+            new()
+            {
+                IsEnabled = true,
+                Name = "Mod 1",
+                LastModified = "Yesterday",
+                Provider = "Provider A",
+                Size = 123456789,
+                Version = "1.0.0",
+            }
+        ];
+        ResourcePacks =
+        [
+            new()
+            {
+                Name = "Resource Pack 1",
+                LastModified = "Yesterday",
+                Provider = "Provider A",
+                Size = 123456789,
+                Version = "1.0.0",
+            }
+        ];
+        ShaderPacks =
+        [
+            new()
+            {
+                Name = "Shader Pack 1",
+                LastModified = "Yesterday",
+                Provider = "Provider A",
+                Size = 123456789,
+                Version = "1.0.0",
+            }
+        ];
+        Worlds =
+        [
+            new()
+            {
+                Name = "World 1",
+                Gamemode = "Survival",
+                LastPlayed = "Today",
+                Size = 123456789,
+            }
+        ];
+        Servers =
+        [
+            new()
+            {
+                Name = "Server 1",
+                Address = "server1.example.com"
+            }
+        ];
+    }
+}

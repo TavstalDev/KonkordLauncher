8e3f2fe0d649026e0693f444715a01ee8a832521
tavstal <tavstal@gmail.com>
2025-08-01 12:42:11 +0200

    Add xml documentation and cultureInfo to Language class

--- Common/Models/Language.cs
+++ Common/Models/Language.cs
@@ -1,38 +1,78 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Globalization;
+using System.Text.Json.Serialization;
 using Newtonsoft.Json;
 
 namespace Tavstal.KonkordLauncher.Common.Models;
 
+/// <summary>
+/// Represents a language with its associated metadata and functionality.
+/// </summary>
 public class Language
 {
+    /// <summary>
+    /// Gets or sets the name of the language.
+    /// </summary>
     [JsonProperty("name"), JsonPropertyName("name")]
     public string Name { get; set; }
+
+    /// <summary>
+    /// Gets or sets the two-letter ISO 639-1 code for the language.
+    /// </summary>
     [JsonProperty("twoLetterCode"), JsonPropertyName("twoLetterCode")]
     public string TwoLetterCode { get; set; }
-    [JsonProperty("threeLetterCode"), JsonPropertyName("threeLetterCode")]
-    public string ThreeLetterCode { get; set; }
+
+    /// <summary>
+    /// Gets or sets the URL for the language's translation file.
+    /// </summary>
     [JsonProperty("url"), JsonPropertyName("url")]
     public string Url { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether this language is the default language.
+    /// </summary>
     [JsonProperty("isDefault"), JsonPropertyName("isDefault")]
     public bool IsDefault { get; set; }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Language"/> class.
+    /// </summary>
     public Language() { }
 
-    public Language(string name, string twoLetterCode, string threeLetterCode, string url)
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Language"/> class with the specified name, codes, and URL.
+    /// </summary>
+    /// <param name="name">The name of the language.</param>
+    /// <param name="twoLetterCode">The two-letter ISO 639-1 code for the language.</param>
+    /// <param name="url">The URL for the language's translation file.</param>
+    public Language(string name, string twoLetterCode, string url)
     {
         Name = name;
         TwoLetterCode = twoLetterCode;
-        ThreeLetterCode = threeLetterCode;
         Url = url;
         IsDefault = false;
     }
 
-    public Language(string name, string twoLetterCode, string threeLetterCode, string url, bool isDefault)
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Language"/> class with the specified name, codes, URL, and default status.
+    /// </summary>
+    /// <param name="name">The name of the language.</param>
+    /// <param name="twoLetterCode">The two-letter ISO 639-1 code for the language.</param>
+    /// <param name="url">The URL for the language's translation file.</param>
+    /// <param name="isDefault">A value indicating whether this language is the default language.</param>
+    public Language(string name, string twoLetterCode, string url, bool isDefault)
     {
         Name = name;
         TwoLetterCode = twoLetterCode;
-        ThreeLetterCode = threeLetterCode;
         Url = url;
         IsDefault = isDefault;
+    }
+
+    /// <summary>
+    /// Gets the <see cref="CultureInfo"/> object associated with the language's two-letter code.
+    /// </summary>
+    /// <returns>A <see cref="CultureInfo"/> object for the language.</returns>
+    public CultureInfo GetCultureInfo()
+    {
+        return new CultureInfo(TwoLetterCode);
     }
 }

5dd6ad8b41412fe8538bf34eb0b2e4c9205990c2
tavstal <tavstal@gmail.com>
2025-08-02 12:26:29 +0200

    Add Instance class to represent Minecraft instances with properties and methods

---
+++ Common/Models/Instance.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+using Tavstal.KonkordLauncher.Core.Enums;
+using Tavstal.KonkordLauncher.Core.Helpers;
+using Tavstal.KonkordLauncher.Core.Models;
+
+namespace Tavstal.KonkordLauncher.Common.Models;
+
+[Serializable]
+public class Instance
+{
+    [JsonPropertyName("name"), JsonProperty("name")]
+    public string Name { get; set; }
+    
+    [JsonPropertyName("group"), JsonProperty("group")]
+    public string? Group { get; set; }
+    
+    [JsonPropertyName("iconPath"), JsonProperty("iconPath")]
+    public string IconPath { get; set; }
+    
+    [JsonPropertyName("minecraftVersion"), JsonProperty("minecraftVersion")]
+    public string MinecraftVersion { get; set; }
+    
+    [JsonPropertyName("customVersion"), JsonProperty("customVersion")]
+    public string CustomVersion { get; set; }
+    
+    [JsonPropertyName("type"), JsonProperty("type")]
+    public EProfileType Type { get; set; }
+    
+    [JsonPropertyName("kind"), JsonProperty("kind")]
+    public EMinecraftKind Kind { get; set; }
+    
+    [JsonProperty("gameDirectory"), JsonPropertyName("gameDirectory")]
+    public string GameDirectory { get; set; }
+    
+    public Instance() { }
+
+    public string GetGameDirectory(string versionsDir)
+    {
+        VersionDetails version = GameHelper.GetVersionDetails(versionsDir, MinecraftVersion, Kind, CustomVersion, GameDirectory);
+        return version.GameDir;
+    }
+
+    public static string GetDefaultJVMArgs()
+    {
+        return "-XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=16M -Djava.net.preferIPv4Stack=true";
+    }
+}

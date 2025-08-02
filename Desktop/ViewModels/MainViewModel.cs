08ad38bb80904867fafc1d97be1793948952d97b
tavstal <tavstal@gmail.com>
2025-08-02 12:27:09 +0200

    Refactor MainViewModel to use CommunityToolkit.Mvvm and add observable properties for current page index and core configuration

--- Desktop/ViewModels/MainViewModel.cs
+++ Desktop/ViewModels/MainViewModel.cs
@@ -1,22 +1,45 @@
 using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
 using ReactiveUI;
+using Tavstal.KonkordLauncher.Common.Helpers;
 using Tavstal.KonkordLauncher.Desktop.Models;
+using Tavstal.KonkordLauncher.Desktop.Models.Config.Launcher;
 using Tavstal.KonkordLauncher.Desktop.Models.Enums;
 
 namespace Tavstal.KonkordLauncher.Desktop.ViewModels;
 
-public class MainViewModel : ViewModelBase
+public partial class MainViewModel : ObservableObject
 {
-    private ESidebarType _currentPageIndex = ESidebarType.Play;
-    public ESidebarType CurrentPageIndex
-    {
-        get => _currentPageIndex;
-        set => this.RaiseAndSetIfChanged(ref _currentPageIndex, value);
-    }
+    private bool _isInitialized;
     
+    [ObservableProperty]
+    private ESidebarType _currentPageIndex;
+
     public ObservableCollection<PlayCardModel> Instances { get; } = [];
     
     public ObservableCollection<NewsCardModel> News { get; } = [];
     
     public ObservableCollection<AccountCardModel> Accounts { get; } = [];
+    
+    [ObservableProperty]
+    private CoreConfigModel _coreConfig;
+
+    public MainViewModel()
+    {
+        _currentPageIndex = ESidebarType.Play;
+        // TODO: Load instances
+        // TODO: Fetch news
+        // TODO: Load accounts
+
+        _coreConfig = new CoreConfigModel(LauncherHelper.GetLauncherSettings());
+        _isInitialized = true;
+    }
+
+    partial void OnCoreConfigChanged(CoreConfigModel value)
+    {
+        if (!_isInitialized)
+            return;
+        
+        // TODO: Save the core config to the settings file
+    }
 }

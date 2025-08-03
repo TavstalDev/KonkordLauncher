c2d1a024c341c30a8e732724292d9d090df56341
tavstal <tavstal@gmail.com>
2025-08-03 13:38:28 +0200

    Add ViewModelBase class to provide reactive property change notifications for view models

---
+++ Desktop/Models/ViewModelBase.cs
@@ -0,0 +1,8 @@
+using ReactiveUI;
+
+namespace Tavstal.KonkordLauncher.Desktop.Models;
+
+/// <summary>
+/// Serves as the base class for all view models in the application, providing reactive property change notifications.
+/// </summary>
+public class ViewModelBase : ReactiveObject;

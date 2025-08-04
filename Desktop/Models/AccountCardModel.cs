c0c004ff405398a2f9f16ee5f258e47e79bf60e1
tavstal <tavstal@gmail.com>
2025-08-04 15:59:30 +0200

    Add Id property to AccountCardModel

--- Desktop/Models/AccountCardModel.cs
+++ Desktop/Models/AccountCardModel.cs
@@ -2,6 +2,7 @@ namespace Tavstal.KonkordLauncher.Desktop.Models;
 
 public class AccountCardModel
 {
+    public string Id { get; set; }
     public string Name { get; set; }
     public string Type { get; set; }
     public bool IsSelected { get; set; }

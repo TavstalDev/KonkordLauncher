4f1e82f32d41682bc5c93bb224c7eac314330737
tavstal <tavstal@gmail.com>
2025-08-04 11:55:23 +0200

    Refactor theme management to use ResourceDictionary for Light and Dark themes

--- Desktop/App.axaml.cs
+++ Desktop/App.axaml.cs
@@ -1,7 +1,14 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Avalonia.Styling;
+using Tavstal.KonkordLauncher.Common.Helpers;
 using Tavstal.KonkordLauncher.Common.Models;
+using Tavstal.KonkordLauncher.Common.Translation;
+using Tavstal.KonkordLauncher.Core.Models;
 
 namespace Tavstal.KonkordLauncher.Desktop;
 
@@ -10,8 +17,10 @@ namespace Tavstal.KonkordLauncher.Desktop;
 /// </summary>
 public partial class App : Application
 {
-    private static PixelSize _screenSize = new PixelSize(1920, 1080);
+    private static readonly CoreLogger _logger = CoreLogger.WithModuleType(typeof(App));
+    private static PixelSize _screenSize = new(1920, 1080);
     public static PixelSize ScreenSize => _screenSize;
+    
     public static decimal ScreenWidth => _screenSize.Width;
     public static decimal ScreenHeight => _screenSize.Height;
     public static void SetScreenSize(PixelSize screenSize)
@@ -19,13 +28,24 @@ public partial class App : Application
         _screenSize = screenSize;
     }
     
-    
     /// <summary>
     /// Initializes the application by loading XAML resources.
     /// </summary>
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
+        OnThemeChanged += ApplyTheme;
+
+        try
+        {
+            var settings = LauncherHelper.GetLauncherSettings();
+            ApplyTheme(settings.Launcher.Theme);
+            //InvokeLanguageChanged(settings.Launcher.Language);
+        }
+        catch (Exception _)
+        {
+            // ignored
+        }
     }
 
     /// <summary>
@@ -62,7 +82,38 @@ public partial class App : Application
     {
         OnThemeChanged?.Invoke(newTheme);
     }
-
+    
+    /// <summary>
+    /// Applies the specified theme to the application by setting the requested theme variant.
+    /// </summary>
+    /// <param name="theme">The theme to apply, either Light or Dark.</param>
+    public void ApplyTheme(EThemeType theme)
+    {
+        try
+        {
+            if (Current == null)
+                return;
+            switch (theme)
+            {
+                case EThemeType.Light:
+                {
+                    RequestedThemeVariant = ThemeVariant.Light;
+                    break;
+                }
+                case EThemeType.Dark:
+                {
+                    RequestedThemeVariant = ThemeVariant.Dark;
+                    break;
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.Exc("Failed to apply theme");
+            _logger.Error(ex);
+        }
+    }
+    
     /// <summary>
     /// Delegate for handling language change events.
     /// </summary>
@@ -80,6 +131,15 @@ public partial class App : Application
     /// <param name="newLanguage">The new language to apply.</param>
     public static void InvokeLanguageChanged(string newLanguage)
     {
+        Task.Run(async () =>
+        {
+            var settings = await LauncherHelper.GetLauncherSettingsAsync();
+            if (!await TranslationManager.EnsureLanguageFileExistsAsync(newLanguage))
+                return;
+            
+            var result = await TranslationManager.ReadTranslationAsync(Path.Combine(settings.Launcher.TranslationsDirectoryPath, $"{newLanguage}.json"));
+            TranslationManager.SetTranslations(newLanguage, result);
+        });
         OnLanguageChanged?.Invoke(newLanguage);
     }
 }

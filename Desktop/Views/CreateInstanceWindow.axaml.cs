a2a612676fca5fb6331fe9c3ff8afb0922a6bea2
tavstal <tavstal@gmail.com>
2025-08-06 21:05:06 +0200

    Implement icon selection and mod type handling in CreateInstanceWindow

--- Desktop/Views/CreateInstanceWindow.axaml.cs
+++ Desktop/Views/CreateInstanceWindow.axaml.cs
@@ -1,6 +1,8 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Tavstal.KonkordLauncher.Core.Enums;
+using Tavstal.KonkordLauncher.Desktop.Models;
 using Tavstal.KonkordLauncher.Desktop.Views.Models;
 
 namespace Tavstal.KonkordLauncher.Desktop.Views;
@@ -25,5 +27,54 @@ public partial class CreateInstanceWindow : Window
         if (DataContext is not CreateInstanceViewModel vm) return;
         
         // TODO
+    }
+
+    private void CustomNoModLoader_Click(object? sender, RoutedEventArgs e)
+    {
+        if (DataContext is not CreateInstanceViewModel vm) return;
+        
+        vm.ModType = EMinecraftKind.VANILLA;
+    }
+
+    private void CustomNeoForge_Click(object? sender, RoutedEventArgs e)
+    {
+        if (DataContext is not CreateInstanceViewModel vm) return;
+        
+        vm.ModType = EMinecraftKind.NEOFORGE;
+    }
+
+    private void CustomForge_Click(object? sender, RoutedEventArgs e)
+    {
+        if (DataContext is not CreateInstanceViewModel vm) return;
+        
+        vm.ModType = EMinecraftKind.FORGE;
+    }
+
+    private void CustomFabric_Click(object? sender, RoutedEventArgs e)
+    {
+        if (DataContext is not CreateInstanceViewModel vm) return;
+        
+        vm.ModType = EMinecraftKind.FABRIC;
+    }
+
+    private void CustomQuilt_Click(object? sender, RoutedEventArgs e)
+    {
+        if (DataContext is not CreateInstanceViewModel vm) return;
+        
+        vm.ModType = EMinecraftKind.QUILT;
+    }
+
+    private async void IconSelector_Click(object? sender, RoutedEventArgs e)
+    {
+        // TODO: Replace async void
+        if (DataContext is not CreateInstanceViewModel vm) 
+            return;
+        
+        IconSelectorWindow window = new();
+        var result = await window.ShowDialog<IconDataModel>(this);
+        // ReSharper disable once ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract
+        if (result == null)
+            return;
+        vm.InstanceIcon = result.Image;
     }
 }

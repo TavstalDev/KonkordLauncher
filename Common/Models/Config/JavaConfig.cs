185331f7d6a11974a8549376130dc0367b48ac18
tavstal <tavstal@gmail.com>
2025-08-06 11:22:42 +0200

    Refactor JavaConfig: rename DefaultJavaPath to JavaPath and remove unused JavaPaths property

--- Common/Models/Config/JavaConfig.cs
+++ Common/Models/Config/JavaConfig.cs
@@ -14,32 +14,27 @@ public class JavaConfig
     [JsonProperty("permaGen"), JsonPropertyName("permaGen")]
     public uint PermaGen { get; set; }
     
-    [JsonProperty("defaultJavaPath"), JsonPropertyName("defaultJavaPath")]
-    public string DefaultJavaPath { get; set; }
+    [JsonProperty("javaPath"), JsonPropertyName("javaPath")]
+    public string JavaPath { get; set; }
     
     [JsonProperty("jvmArguments"), JsonPropertyName("jvmArguments")]
     public string JvmArguments { get; set; }
-    
-    [JsonProperty("javaPaths"), JsonPropertyName("javaPaths")]
-    public Dictionary<int, List<string>> JavaPaths { get; set; }
 
     public JavaConfig()
     {
         MinMemory = 1024;
         MaxMemory = 4096;
         PermaGen = 128;
-        DefaultJavaPath = string.Empty;
+        JavaPath = string.Empty;
         JvmArguments = string.Empty;
-        JavaPaths = new Dictionary<int, List<string>>();
     }
 
-    public JavaConfig(uint minMemory, uint maxMemory, uint permaGen, string defaultJavaPath, string jvmArguments, Dictionary<int, List<string>> javaPaths)
+    public JavaConfig(uint minMemory, uint maxMemory, uint permaGen, string defaultJavaPath, string jvmArguments)
     {
         MinMemory = minMemory;
         MaxMemory = maxMemory;
         PermaGen = permaGen;
-        DefaultJavaPath = defaultJavaPath;
+        JavaPath = defaultJavaPath;
         JvmArguments = jvmArguments;
-        JavaPaths = javaPaths;
     }
 }

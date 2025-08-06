b3605c7db818d13f368b774a450f4dd8c4b6804b
tavstal <tavstal@gmail.com>
2025-08-07 01:29:24 +0200

    Refactor CreateInstanceWindow to improve mod loader selection and enhance instance creation logic

--- Desktop/Views/CreateInstanceWindow.axaml.cs
+++ Desktop/Views/CreateInstanceWindow.axaml.cs
@@ -1,14 +1,29 @@
+using System.Linq;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Tavstal.KonkordLauncher.Common.Helpers;
+using Tavstal.KonkordLauncher.Common.Models;
+using Tavstal.KonkordLauncher.Common.Models.InstanceConfig;
 using Tavstal.KonkordLauncher.Core.Enums;
+using Tavstal.KonkordLauncher.Core.Helpers;
 using Tavstal.KonkordLauncher.Desktop.Models;
+using Tavstal.KonkordLauncher.Desktop.Models.Enums;
 using Tavstal.KonkordLauncher.Desktop.Views.Models;
 
 namespace Tavstal.KonkordLauncher.Desktop.Views;
 
+/// <summary>
+/// Represents the window for creating a new instance in the application.
+/// Initializes the UI components, attaches Avalonia Dev Tools in debug mode,
+/// and sets the DataContext to a new <see cref="CreateInstanceViewModel"/>.
+/// </summary>
 public partial class CreateInstanceWindow : Window
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CreateInstanceWindow"/> class.
+    /// Sets up the UI components, attaches dev tools in debug mode, and assigns the DataContext.
+    /// </summary>
     public CreateInstanceWindow()
     {
         InitializeComponent();
@@ -19,62 +34,128 @@ public partial class CreateInstanceWindow : Window
 #endif
 
         this.DataContext = new CreateInstanceViewModel();
-        
     }
 
-    private void Control_OnLoaded(object? sender, RoutedEventArgs e)
-    {
-        if (DataContext is not CreateInstanceViewModel vm) return;
-        
-        // TODO
-    }
-
+    #region Custom - Mod Loader Select
+    /// <summary>
+    /// Handles the click event for the "No Mod Loader" option.
+    /// Sets the ModLoaderType of the ViewModel to VANILLA.
+    /// </summary>
+    /// <param name="sender">The source of the event.</param>
+    /// <param name="e">The event data.</param>
     private void CustomNoModLoader_Click(object? sender, RoutedEventArgs e)
     {
         if (DataContext is not CreateInstanceViewModel vm) return;
-        
-        vm.ModType = EMinecraftKind.VANILLA;
+
+        vm.ModLoaderType = EMinecraftKind.VANILLA;
     }
 
+    /// <summary>
+    /// Handles the click event for the "NeoForge" mod loader option.
+    /// Sets the ModLoaderType of the ViewModel to NEOFORGE.
+    /// </summary>
+    /// <param name="sender">The source of the event.</param>
+    /// <param name="e">The event data.</param>
     private void CustomNeoForge_Click(object? sender, RoutedEventArgs e)
     {
         if (DataContext is not CreateInstanceViewModel vm) return;
-        
-        vm.ModType = EMinecraftKind.NEOFORGE;
+
+        vm.ModLoaderType = EMinecraftKind.NEOFORGE;
     }
 
+    /// <summary>
+    /// Handles the click event for the "Forge" mod loader option.
+    /// Sets the ModLoaderType of the ViewModel to FORGE.
+    /// </summary>
+    /// <param name="sender">The source of the event.</param>
+    /// <param name="e">The event data.</param>
     private void CustomForge_Click(object? sender, RoutedEventArgs e)
     {
         if (DataContext is not CreateInstanceViewModel vm) return;
-        
-        vm.ModType = EMinecraftKind.FORGE;
+
+        vm.ModLoaderType = EMinecraftKind.FORGE;
     }
 
+    /// <summary>
+    /// Handles the click event for the "Fabric" mod loader option.
+    /// Sets the ModLoaderType of the ViewModel to FABRIC.
+    /// </summary>
+    /// <param name="sender">The source of the event.</param>
+    /// <param name="e">The event data.</param>
     private void CustomFabric_Click(object? sender, RoutedEventArgs e)
     {
         if (DataContext is not CreateInstanceViewModel vm) return;
-        
-        vm.ModType = EMinecraftKind.FABRIC;
+
+        vm.ModLoaderType = EMinecraftKind.FABRIC;
     }
 
+    /// <summary>
+    /// Handles the click event for the "Quilt" mod loader option.
+    /// Sets the ModLoaderType of the ViewModel to QUILT.
+    /// </summary>
+    /// <param name="sender">The source of the event.</param>
+    /// <param name="e">The event data.</param>
     private void CustomQuilt_Click(object? sender, RoutedEventArgs e)
     {
         if (DataContext is not CreateInstanceViewModel vm) return;
-        
-        vm.ModType = EMinecraftKind.QUILT;
-    }
 
+        vm.ModLoaderType = EMinecraftKind.QUILT;
+    }
+    #endregion
+
+    /// <summary>
+    /// Handles the click event for the icon selector.
+    /// Opens the IconSelectorWindow dialog and sets the selected icon in the ViewModel if one is chosen.
+    /// </summary>
+    /// <param name="sender">The source of the event.</param>
+    /// <param name="e">The event data.</param>
     private async void IconSelector_Click(object? sender, RoutedEventArgs e)
     {
         // TODO: Replace async void
-        if (DataContext is not CreateInstanceViewModel vm) 
+        if (DataContext is not CreateInstanceViewModel vm)
             return;
-        
+
         IconSelectorWindow window = new();
         var result = await window.ShowDialog<IconDataModel>(this);
         // ReSharper disable once ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract
         if (result == null)
             return;
         vm.InstanceIcon = result.Image;
+        vm.InstanceIconPath = result.Path;
+    }
+
+    private void CreateInstance_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if (DataContext is not CreateInstanceViewModel vm)
+            return;
+        
+        var instances = LauncherHelper.GetInstances();
+        if (instances.Any(x => x.Name == vm.InstanceName))
+        {
+            AlertWindow alertWindow = new("Instance already exists",
+                "An instance with this name already exists. Please choose a different name.",
+                EAlertType.Error);
+            alertWindow.ShowDialog(this);
+            return;
+        }
+        
+        instances.Add(new Instance()
+        {
+            Name = vm.InstanceName,
+            Kind = vm.ModLoaderType,
+            Group = "none",
+            MinecraftVersion = vm.SelectedMinecraftVersion.Id,
+            CustomVersion = vm.SelectedModLoader?.Version ?? string.Empty,
+            IconPath = vm.InstanceIconPath ?? string.Empty,
+            Config = new InstanceConfig()
+        });
+        JsonHelper.WriteJsonFile(PathHelper.LauncherInstancesPath, instances);
+        App.InvokeInstancesChanged();
+        this.Close();
+    }
+
+    private void CancelInstance_OnClick(object? sender, RoutedEventArgs e)
+    {
+        this.Close();
     }
 }

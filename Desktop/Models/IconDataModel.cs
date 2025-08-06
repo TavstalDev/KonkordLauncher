0cb2a67835144b58dc467c6e9ecfee260f2c39d1
tavstal <tavstal@gmail.com>
2025-08-06 21:01:58 +0200

    Add IconDataModel class to represent icon data with name, path, and image

---
+++ Desktop/Models/IconDataModel.cs
@@ -0,0 +1,38 @@
+using Avalonia.Media.Imaging;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Tavstal.KonkordLauncher.Desktop.Models;
+
+/// <summary>
+/// Represents a data model for an icon, containing its name and image.
+/// </summary>
+public partial class IconDataModel : ObservableObject
+{
+    /// <summary>
+    /// The name of the icon.
+    /// </summary>
+    [ObservableProperty] private string _name;
+
+    /// <summary>
+    /// The file path associated with the icon.
+    /// </summary>
+    [ObservableProperty] private string _path;
+    
+    /// <summary>
+    /// The image associated with the icon.
+    /// </summary>
+    [ObservableProperty] private Bitmap _image;
+    
+    /// <summary>
+    /// Initializes a new instance of the <see cref="IconDataModel"/> class.
+    /// </summary>
+    /// <param name="name">The name of the icon.</param>
+    /// <param name="path">The file path of the icon.</param>
+    /// <param name="image">The image associated with the icon.</param>
+    public IconDataModel(string name, string path, Bitmap image)
+    {
+        _name = name;
+        _path = path;
+        _image = image;
+    }
+}

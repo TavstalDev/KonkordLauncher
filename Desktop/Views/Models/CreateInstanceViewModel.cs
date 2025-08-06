1b5af2e9cbfee023ec348ad964fddf0a66ba1ff2
tavstal <tavstal@gmail.com>
2025-08-06 21:04:51 +0200

    Refactor CreateInstanceViewModel to use CommunityToolkit.Mvvm for property management and enhance version filtering logic. Implement minecraft vanilla & mod loader version selecting

--- Desktop/Views/Models/CreateInstanceViewModel.cs
+++ Desktop/Views/Models/CreateInstanceViewModel.cs
@@ -1,101 +1,144 @@
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using ReactiveUI;
-using Tavstal.KonkordLauncher.Desktop.Models;
+using System.Linq;
+using Avalonia.Media.Imaging;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Tavstal.KonkordLauncher.Core.Enums;
+using Tavstal.KonkordLauncher.Core.Helpers;
+using Tavstal.KonkordLauncher.Core.Models.ModLoaders;
+using Tavstal.KonkordLauncher.Core.Models.MojangApi;
+using Tavstal.KonkordLauncher.Desktop.Helpers;
 using Tavstal.KonkordLauncher.Desktop.Models.Instance;
 
 namespace Tavstal.KonkordLauncher.Desktop.Views.Models;
 
-public class CreateInstanceViewModel : ViewModelBase
+public partial class CreateInstanceViewModel : ObservableObject
 {
-    private static readonly ReverseMarkdown.Converter _converter = new();
-    
+    private readonly VersionManifest _vanillaManifest;
+    private readonly ReverseMarkdown.Converter _converter = new();
+
     #region Custom
     
-    private string _searchQuery;
-    public string SearchQuery
-    {
-        get => _searchQuery;
-        set => this.RaiseAndSetIfChanged(ref _searchQuery, value);
-    }
+    [ObservableProperty] private string _instanceName = string.Empty;
+    [ObservableProperty] private string _instanceGroup = string.Empty;
+    [ObservableProperty] private Bitmap? _instanceIcon;
     
-    private bool _showReleases;
-    public bool ShowReleases
-    {
-        get => _showReleases;
-        set => this.RaiseAndSetIfChanged(ref _showReleases, value);
-    }
-    private bool _showSnapshots;
-    public bool ShowSnapshots
-    {
-        get => _showSnapshots;
-        set => this.RaiseAndSetIfChanged(ref _showSnapshots, value);
-    }
+    #region Vanilla
+    [ObservableProperty] [NotifyPropertyChangedFor(nameof(VersionResult))] private string _searchQuery = string.Empty;
+
+    [ObservableProperty] [NotifyPropertyChangedFor(nameof(VersionResult))] private bool _showReleases = true;
+
+    [ObservableProperty] [NotifyPropertyChangedFor(nameof(VersionResult))] private bool _showSnapshots;
+
+    [ObservableProperty] [NotifyPropertyChangedFor(nameof(VersionResult))] private bool _showAlphas;
+
+    [ObservableProperty] [NotifyPropertyChangedFor(nameof(VersionResult))] private bool _showBetas;
+
+    [ObservableProperty] [NotifyPropertyChangedFor(nameof(VersionResult))] private bool _showExperiments;
+
+    public List<MinecraftVersion> VersionResult => _vanillaManifest.Versions.FindAll(x =>
+        (string.IsNullOrEmpty(SearchQuery) || x.Id.Contains(SearchQuery)) &&
+        (x.Type != "release" || ShowReleases) &&
+        (x.Type != "snapshot" || ShowSnapshots) &&
+        (x.Type != "old_alpha" || ShowAlphas) &&
+        (x.Type != "old_beta" || ShowBetas) &&
+        (x.Type != "experiment" || ShowExperiments));
     
-    private bool _showAlphas;
-    public bool ShowAlphas
+    [ObservableProperty] private MinecraftVersion _selectedMinecraftVersion;
+    #endregion
+    #region  Mod Loader
+    [ObservableProperty] [NotifyPropertyChangedFor(nameof(ModVersionResult))] private string _modSearchQuery = string.Empty;
+    [ObservableProperty] [NotifyPropertyChangedFor(nameof(ModVersionResult))] private EMinecraftKind _modType = EMinecraftKind.VANILLA;
+
+    public List<IModManifest> ModVersionResult
     {
-        get => _showAlphas;
-        set => this.RaiseAndSetIfChanged(ref _showAlphas, value);
-    }
-    
-    private bool _showBetas;
-    public bool ShowBetas
-    {
-        get => _showBetas;
-        set => this.RaiseAndSetIfChanged(ref _showBetas, value);
-    }
-    
-    private bool _showExperiments;
-    public bool ShowExperiments
-    {
-        get => _showExperiments;
-        set => this.RaiseAndSetIfChanged(ref _showExperiments, value);
-    }
-    
-    public ObservableCollection<VersionModel> Versions { get; set; } =
-    [
-        new VersionModel
+        get
         {
-            Version = "1.20.2",
-            ReleaseDate = "2023-10-01",
-            Type = "Release",
+            List<IModManifest> result = [];
+            switch (ModType)
+            {
+                case EMinecraftKind.VANILLA:
+                    break;
+                case EMinecraftKind.NEOFORGE:
+                {
+                    var data = ManifestHelper.GetNeoForgeManifest();
+                    if (data == null)
+                        return [];
+                    foreach (var version in data)
+                    {
+                        if (version.GameVersion != SelectedMinecraftVersion.Id)
+                            continue;
+                        
+                        if (!string.IsNullOrEmpty(ModSearchQuery) || !version.Version.Contains(ModSearchQuery))
+                            continue;
+                        
+                        result.Add(version);
+                    }
+                    break;
+                }
+                case EMinecraftKind.FORGE:
+                {
+                    var data = ManifestHelper.GetForgeManifest();
+                    if (data == null)
+                        return [];
+                    foreach (var version in data)
+                    {
+                        if (version.GameVersion != SelectedMinecraftVersion.Id)
+                            continue;
+                        
+                        if (!string.IsNullOrEmpty(ModSearchQuery) || !version.Version.Contains(ModSearchQuery))
+                            continue;
+                        
+                        result.Add(version);
+                    }
+                    break;
+                }
+                case EMinecraftKind.FABRIC:
+                {
+                    result = ManifestHelper.GetFabricManifest()!;
+                    if (result == null)
+                        return [];
+                    
+                    if (!string.IsNullOrEmpty(ModSearchQuery))
+                        result = result.FindAll(x => x.Version.Contains(ModSearchQuery));
+                    break;
+                }
+                case EMinecraftKind.QUILT:
+                {
+                    result = ManifestHelper.GetQuiltManifest()!;
+                    if (result == null)
+                        return [];
+                    
+                    if (!string.IsNullOrEmpty(ModSearchQuery))
+                        result = result.FindAll(x => x.Version.Contains(ModSearchQuery));
+                    break;
+                }
+            }
+            
+            return result;
         }
-    ];
+    }
+    #endregion
     #endregion
     
     #region Modpack
 
-    public ObservableCollection<ModPackModel> Modpacks { get; set; } = new();
-
+    [ObservableProperty] private ObservableCollection<ModPackModel> _modpacks;
     
-    private ModPackModel? _selectedModpack;
-    public ModPackModel? SelectedModpack
-    {
-        get => _selectedModpack;
-        set
-        {
-            this.RaiseAndSetIfChanged(ref _selectedModpack, value);
-            if (value != null)
-            {
-                ModpackPreview = _converter.Convert(value.RawPage);
-            }
-            else
-            {
-                ModpackPreview = _converter.Convert("<p>Select a modpack to see its preview.</p>");
-            }
-        }
-    }
+    [ObservableProperty] [NotifyPropertyChangedFor(nameof(ModpackPreview))] private ModPackModel? _selectedModpack;
     
-    private string _modpackPreview = _converter.Convert(@"<p>Select a modpack to see its preview.</p>");
-    public string ModpackPreview 
-    {
-        get => _modpackPreview;
-        set => this.RaiseAndSetIfChanged(ref _modpackPreview, value);
-    }
+    public string ModpackPreview => SelectedModpack == null ? _converter.Convert(@"<p>Select a modpack to see its preview.</p>") : _converter.Convert(SelectedModpack.RawPage);
 
     #endregion
 
     #region Import
 
     #endregion
+
+    public CreateInstanceViewModel()
+    {
+        _vanillaManifest = ManifestHelper.GetMinecraftManifest()!;
+        _selectedMinecraftVersion = VersionResult.First();
+        InstanceIcon = ImageHelper.Load("avares://Desktop/Assets/Icons/dirt.png").Result;
+    }
 }

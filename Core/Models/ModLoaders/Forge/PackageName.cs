f2955f64493daac41f20af6a3c9c2c76a580a900
tavstal <tavstal@gmail.com>
2025-08-09 22:05:40 +0200

    Enhance PackageName and ProcessorJarFile classes; add XML documentation for improved clarity and maintainability

--- Core/Models/ModLoaders/Forge/PackageName.cs
+++ Core/Models/ModLoaders/Forge/PackageName.cs
@@ -1,18 +1,48 @@
 ﻿namespace Tavstal.KonkordLauncher.Core.Models.ModLoaders.Forge;
 
-// Source: https://github.com/CmlLib/CmlLib.Core.Installer.Forge
+/// <summary>
+/// Represents a package name used in the Forge mod loader.
+/// Provides methods to parse and manipulate package data.
+/// </summary>
+/// <remarks>
+/// Source: https://github.com/CmlLib/CmlLib.Core.Installer.Forge
+/// </remarks>
 public class PackageName
 {
+    /// <summary>
+    /// Stores the components of the package name.
+    /// </summary>
     private readonly string[] names;
 
+    /// <summary>
+    /// Gets the component of the package name at the specified index.
+    /// </summary>
+    /// <param name="index">The index of the component to retrieve.</param>
+    /// <returns>The component at the specified index.</returns>
     public string this[int index] => names[index];
 
+    /// <summary>
+    /// Gets the package identifier (first component of the package name).
+    /// </summary>
     public string Package => names[0];
 
+    /// <summary>
+    /// Gets the name of the package (second component of the package name).
+    /// </summary>
     public string Name => names[1];
 
+    /// <summary>
+    /// Gets the version of the package (third component of the package name).
+    /// </summary>
     public string Version => names[2];
 
+    /// <summary>
+    /// Parses a package name string into a <see cref="PackageName"/> instance.
+    /// </summary>
+    /// <param name="name">The package name string to parse.</param>
+    /// <returns>A <see cref="PackageName"/> instance representing the parsed package name.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if the provided name is null.</exception>
+    /// <exception cref="ArgumentException">Thrown if the provided name is invalid.</exception>
     public static PackageName Parse(string name)
     {
         if (name == null)
@@ -29,21 +59,40 @@ public class PackageName
         return new PackageName(array);
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PackageName"/> class with the specified components.
+    /// </summary>
+    /// <param name="names">The components of the package name.</param>
     private PackageName(string[] names)
     {
         this.names = names;
     }
 
+    /// <summary>
+    /// Gets the file path for the package with an optional native identifier and default extension.
+    /// </summary>
+    /// <returns>The file path for the package.</returns>
     public string GetPath()
     {
         return GetPath("");
     }
 
+    /// <summary>
+    /// Gets the file path for the package with the specified native identifier and default extension.
+    /// </summary>
+    /// <param name="nativeId">The native identifier to include in the file path.</param>
+    /// <returns>The file path for the package.</returns>
     public string GetPath(string? nativeId)
     {
         return GetPath(nativeId, "jar");
     }
 
+    /// <summary>
+    /// Gets the file path for the package with the specified native identifier and file extension.
+    /// </summary>
+    /// <param name="nativeId">The native identifier to include in the file path.</param>
+    /// <param name="extension">The file extension to use.</param>
+    /// <returns>The file path for the package.</returns>
     public string GetPath(string? nativeId, string extension)
     {
         string text = string.Join("-", names, 1, names.Length - 1);
@@ -53,14 +102,22 @@ public class PackageName
         }
 
         text = text + "." + extension;
-        return Path.Combine(GetDirectory(), text);
+        return Path.Combine(GetDirectory(), text).Replace("@jar", ""); // NeoForge fix
     }
 
+    /// <summary>
+    /// Gets the directory path for the package.
+    /// </summary>
+    /// <returns>The directory path for the package.</returns>
     public string GetDirectory()
     {
         return Path.Combine(Package.Replace(".", "/"), Name, Version);
     }
 
+    /// <summary>
+    /// Gets the class path for the package.
+    /// </summary>
+    /// <returns>The class path for the package.</returns>
     public string GetClassPath()
     {
         return Package + "." + Name;
--- Core/Models/ModLoaders/Forge/ProcessorJarFile.cs
+++ Core/Models/ModLoaders/Forge/ProcessorJarFile.cs
@@ -3,19 +3,38 @@ using ICSharpCode.SharpZipLib.Zip;
 
 namespace Tavstal.KonkordLauncher.Core.Models.ModLoaders.Forge;
 
-// Source: https://github.com/CmlLib/CmlLib.Core.Installer.Forge
+/// <summary>
+/// Represents a processor for handling JAR files in the Forge mod loader.
+/// Provides functionality to extract and parse the manifest file from a JAR archive.
+/// </summary>
+/// <remarks>
+/// Source: https://github.com/CmlLib/CmlLib.Core.Installer.Forge
+/// </remarks>
 public class ProcessorJarFile
 {
+    /// <summary>
+    /// Gets the path to the JAR file being processed.
+    /// </summary>
     public string Path { get; private set; }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ProcessorJarFile"/> class with the specified file path.
+    /// </summary>
+    /// <param name="path">The path to the JAR file.</param>
     public ProcessorJarFile(string path)
     {
         Path = path;
     }
 
+    /// <summary>
+    /// Extracts and parses the manifest file from the JAR archive.
+    /// </summary>
+    /// <returns>
+    /// A dictionary containing key-value pairs from the manifest file, or <c>null</c> if the manifest is not found.
+    /// </returns>
     public Dictionary<string, string?>? GetManifest()
     {
-        string text = null;
+        string? text = null;
         using (FileStream baseInputStream = File.OpenRead(Path))
         {
             using ZipInputStream zipInputStream = new ZipInputStream(baseInputStream);
@@ -23,18 +42,16 @@ public class ProcessorJarFile
             {
                 if (nextEntry.Name == "META-INF/MANIFEST.MF")
                 {
-                    text = readStreamString(zipInputStream);
+                    text = ReadStreamString(zipInputStream);
                     break;
                 }
             }
         }
 
         if (string.IsNullOrEmpty(text))
-        {
             return null;
-        }
 
-        Dictionary<string, string> dictionary = new Dictionary<string, string>();
+        Dictionary<string, string?> dictionary = new Dictionary<string, string?>();
         string[] array = text.Split('\n');
         foreach (string text2 in array)
         {
@@ -63,7 +80,12 @@ public class ProcessorJarFile
         return dictionary;
     }
 
-    private static string readStreamString(Stream s)
+    /// <summary>
+    /// Reads the content of a stream and converts it to a string using UTF-8 encoding.
+    /// </summary>
+    /// <param name="s">The stream to read from.</param>
+    /// <returns>The string representation of the stream's content.</returns>
+    private static string ReadStreamString(Stream s)
     {
         StringBuilder stringBuilder = new StringBuilder();
         byte[] array = new byte[1024];
@@ -71,9 +93,7 @@ public class ProcessorJarFile
         {
             int num = s.Read(array, 0, array.Length);
             if (num == 0)
-            {
                 break;
-            }
 
             stringBuilder.Append(Encoding.UTF8.GetString(array, 0, num));
         }

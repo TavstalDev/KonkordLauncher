8fc9b3e5254bd8f93146f95491ac06ba1ab90018
tavstal <tavstal@gmail.com>
2025-08-10 22:01:50 +0200

    Refactor EditInstanceViewModel to enhance initialization and property change handling

--- Desktop/Views/Models/EditInstanceViewModel.cs
+++ Desktop/Views/Models/EditInstanceViewModel.cs
@@ -1,83 +1,167 @@
 using System.Collections.ObjectModel;
-using Tavstal.KonkordLauncher.Desktop.Models;
-using Tavstal.KonkordLauncher.Desktop.Models.Avalonia;
+using System.ComponentModel;
+using System.Linq;
+using Avalonia.Controls;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Tavstal.KonkordLauncher.Common.Helpers;
+using Tavstal.KonkordLauncher.Common.Models;
+using Tavstal.KonkordLauncher.Common.Models.Config;
+using Tavstal.KonkordLauncher.Common.Models.InstanceConfig;
+using Tavstal.KonkordLauncher.Core.Helpers;
+using Tavstal.KonkordLauncher.Core.Models;
+using Tavstal.KonkordLauncher.Desktop.Models.Config.Instance;
 using Tavstal.KonkordLauncher.Desktop.Models.Instance;
 
 namespace Tavstal.KonkordLauncher.Desktop.Views.Models;
 
-public class EditInstanceViewModel : ViewModelBase
+public partial class EditInstanceViewModel : ObservableObject
 {
-    public ObservableCollection<ModModel> Mods { get; set; }
-    
-    public ObservableCollection<ResourcePackModel> ResourcePacks { get; set; }
-    
-    public ObservableCollection<ShaderPackModel> ShaderPacks { get; set; }
-    
-    public ObservableCollection<WorldModel> Worlds { get; set; }
-    
-    public ObservableCollection<ServerModel> Servers { get; set; }
-    
-    public ObservableCollection<ScreenshotModel> Screenshots { get; set; }
+    private readonly bool _isInitialized;
+    private readonly Window _parentWindow;
+    private readonly CoreLogger _logger = CoreLogger.WithModuleType(typeof(EditInstanceViewModel));
+    private readonly string _instanceId;
 
-    public ObservableDictionary<string, string> EnvironmentVariables { get; set; } = new();
+    public ObservableCollection<ModModel> Mods { get; set; } = [];
     
-    public EditInstanceViewModel()
+    public ObservableCollection<ResourcePackModel> ResourcePacks { get; set; }  = [];
+    
+    public ObservableCollection<ShaderPackModel> ShaderPacks { get; set; }  = [];
+    
+    public ObservableCollection<WorldModel> Worlds { get; set; }  = [];
+    
+    public ObservableCollection<ServerModel> Servers { get; set; }  = [];
+    
+    public ObservableCollection<ScreenshotModel> Screenshots { get; set; }  = [];
+    
+    [ObservableProperty] private InstanceConfigModel _instanceConfig;
+    
+    public EditInstanceViewModel(Window parent, string instanceId, InstanceConfig instanceConfig)
     {
-        Mods =
-        [
-            new()
-            {
-                IsEnabled = true,
-                Name = "Mod 1",
-                LastModified = "Yesterday",
-                Provider = "Provider A",
-                Size = 123456789,
-                Version = "1.0.0",
-            }
-        ];
-        ResourcePacks =
-        [
-            new()
-            {
-                Name = "Resource Pack 1",
-                LastModified = "Yesterday",
-                Provider = "Provider A",
-                Size = 123456789,
-                Version = "1.0.0",
-            }
-        ];
-        ShaderPacks =
-        [
-            new()
-            {
-                Name = "Shader Pack 1",
-                LastModified = "Yesterday",
-                Provider = "Provider A",
-                Size = 123456789,
-                Version = "1.0.0",
-            }
-        ];
-        Worlds =
-        [
-            new()
-            {
-                Name = "World 1",
-                Gamemode = "Survival",
-                LastPlayed = "Today",
-                Size = 123456789,
-            }
-        ];
-        Servers =
-        [
-            new()
-            {
-                Name = "Server 1",
-                Address = "server1.example.com"
-            }
-        ];
-        EnvironmentVariables =
-        [
-            new ("JAVA_HOME", "C:\\Program Files\\Java\\jdk-17"),
-        ];
+        if (Design.IsDesignMode)
+        {
+            _instanceConfig = new InstanceConfigModel(instanceConfig);
+            return;
+        }
+        
+        _parentWindow = parent;
+        _instanceId = instanceId;
+        _instanceConfig = new InstanceConfigModel(instanceConfig);
+        _isInitialized = true;
+        SubscribeToConfigChildren(_instanceConfig);
     }
+
+    #region Settings
+    
+    private void SubscribeToConfigChildren(InstanceConfigModel config)
+    {
+        config.Game.PropertyChanged += OnChildConfigPropertyChanged;
+        config.Java.PropertyChanged += OnChildConfigPropertyChanged;
+        config.Commands.PropertyChanged += OnChildConfigPropertyChanged;
+        config.Environment.PropertyChanged += OnChildConfigPropertyChanged;
+        config.Misc.PropertyChanged += OnChildConfigPropertyChanged;
+    }
+
+    private void UnsubscribeFromConfigChildren(InstanceConfigModel config)
+    {
+        config.Game.PropertyChanged -= OnChildConfigPropertyChanged;
+        config.Java.PropertyChanged -= OnChildConfigPropertyChanged;
+        config.Commands.PropertyChanged -= OnChildConfigPropertyChanged;
+        config.Environment.PropertyChanged -= OnChildConfigPropertyChanged;
+        config.Misc.PropertyChanged -= OnChildConfigPropertyChanged;
+    }
+    
+    partial void OnInstanceConfigChanged(InstanceConfigModel? oldValue, InstanceConfigModel newValue)
+    {
+        _logger.Debug("InstanceConfig changed with old and new value. Unsubscribing from old, subscribing to new.");
+
+        if (oldValue != null)
+            UnsubscribeFromConfigChildren(oldValue);
+
+        SubscribeToConfigChildren(newValue);
+
+        if (!_isInitialized)
+            return;
+        SaveCoreConfigToFile(newValue);
+    }
+
+
+    private void OnChildConfigPropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (!_isInitialized)
+            return;
+        _logger.Debug($"Inner property '{e.PropertyName}' changed on {sender?.GetType().Name}. Saving to file...");
+        SaveCoreConfigToFile(InstanceConfig);
+    }
+
+
+    private void SaveCoreConfigToFile(InstanceConfigModel newValue)
+    {
+        if (newValue.Java.MinMemory > newValue.Java.MaxMemory)
+            newValue.Java.MinMemory = newValue.Java.MaxMemory;
+
+        var instances = LauncherHelper.GetInstances();
+        int index = 0;
+        Instance? instanceToSave = null;
+        foreach (var instance in instances)
+        {
+            if (instance.Id == _instanceId)
+            {
+                instanceToSave = instance;
+                break;
+            }
+
+            index++;
+        }
+
+        if (instanceToSave == null)
+            return;
+
+        instanceToSave.Config = new InstanceConfig()
+        {
+            Java = new JavaConfig()
+            {
+                MinMemory = newValue.Java.MinMemory,
+                MaxMemory = newValue.Java.MaxMemory,
+                PermaGen = newValue.Java.PermaGen,
+                JavaPath = newValue.Java.DefaultJavaPath,
+                JvmArguments = newValue.Java.JvmArguments
+            },
+            Game = new InstanceGameConfig()
+            {
+                StartMaximized = newValue.Game.StartMaximized,
+                WindowHeight = newValue.Game.WindowHeight,
+                WindowWidth = newValue.Game.WindowWidth,
+                ShowConsoleWhileGameRunning = newValue.Game.ShowConsoleWhileGameRunning,
+                ShowConsoleWhenGameCrashes = newValue.Game.ShowConsoleWhenGameCrashes,
+                CloseConsoleOnGameExit = newValue.Game.CloseConsoleOnGameExit,
+                EnableFeralGameMode = newValue.Game.EnableFeralGameMode,
+                EnableMangoHud = newValue.Game.EnableMangoHud,
+                UseDedicatedGpu = newValue.Game.UseDedicatedGpu,
+            },
+            Commands = new InstanceCommandsConfig()
+            {
+                PreLaunchCommand = newValue.Commands.PreLaunchCommand,
+                WrapperCommand = newValue.Commands.WrapperCommand,
+                PostExitCommand = newValue.Commands.PostExitCommand,
+            },
+            EnableEnvironment = newValue.EnableEnvironment,
+            Environment = newValue.Environment.ToDictionary(),
+            Misc = new InstanceMiscConfig()
+            {
+                UseCustomGlfw = newValue.Misc.UseCustomGlfw,
+                CustomGlfwPath = newValue.Misc.CustomGlfwPath,
+                UseCustomOpenAL = newValue.Misc.UseCustomOpenAL,
+                CustomOpenALPath = newValue.Misc.CustomOpenALPath,
+                AccountId = newValue.Misc.AccountId,
+                OverrideAccount = newValue.Misc.OverrideAccount,
+                JoinServerOnLaunch = newValue.Misc.JoinServerOnLaunch,
+                ServerAddress = newValue.Misc.ServerAddress,
+            }
+        };
+        instances[index] = instanceToSave;
+
+        JsonHelper.WriteJsonFile(PathHelper.LauncherInstancesPath, instances);
+        App.InvokeInstancesChanged();
+    }
+    #endregion
 }

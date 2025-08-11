0fbdfaf0b9abe5bea5938ae1147df76bb5131e27
tavstal <tavstal@gmail.com>
2025-08-11 15:23:08 +0200

    Refactor EditInstanceViewModel to handle environment variable collection changes and improve configuration management

--- Desktop/Views/Models/EditInstanceViewModel.cs
+++ Desktop/Views/Models/EditInstanceViewModel.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Linq;
 using Avalonia.Controls;
@@ -18,60 +20,85 @@ namespace Tavstal.KonkordLauncher.Desktop.Views.Models;
 public partial class EditInstanceViewModel : ObservableObject
 {
     private readonly bool _isInitialized;
-    private readonly Window _parentWindow;
+    private readonly EditInstanceWindow _parentWindow;
     private readonly CoreLogger _logger = CoreLogger.WithModuleType(typeof(EditInstanceViewModel));
     private readonly string _instanceId;
     public bool IsLinux => OSHelper.GetOperatingSystem() == EOperatingSystem.Linux;
+    public List<Account> Accounts => LauncherHelper.GetAccountData().Accounts;
 
     public ObservableCollection<ModModel> Mods { get; set; } = [];
+
+    public ObservableCollection<ResourcePackModel> ResourcePacks { get; set; } = [];
+
+    public ObservableCollection<ShaderPackModel> ShaderPacks { get; set; } = [];
+
+    public ObservableCollection<WorldModel> Worlds { get; set; } = [];
+
+    public ObservableCollection<ServerModel> Servers { get; set; } = [];
+
+    public ObservableCollection<ScreenshotModel> Screenshots { get; set; } = [];
+
+    [ObservableProperty] [NotifyPropertyChangedFor(nameof(CanRemoveEnvironmentVariable))] private InstanceConfigModel _instanceConfig;
+
+    [ObservableProperty] [NotifyPropertyChangedFor(nameof(CanRemoveEnvironmentVariable))] private int? _selectedEnvironmentVariableIndex;
     
-    public ObservableCollection<ResourcePackModel> ResourcePacks { get; set; }  = [];
-    
-    public ObservableCollection<ShaderPackModel> ShaderPacks { get; set; }  = [];
-    
-    public ObservableCollection<WorldModel> Worlds { get; set; }  = [];
-    
-    public ObservableCollection<ServerModel> Servers { get; set; }  = [];
-    
-    public ObservableCollection<ScreenshotModel> Screenshots { get; set; }  = [];
-    
-    [ObservableProperty] private InstanceConfigModel _instanceConfig;
-    
-    public EditInstanceViewModel(Window parent, string instanceId, InstanceConfig instanceConfig)
+    public bool CanRemoveEnvironmentVariable => SelectedEnvironmentVariableIndex.HasValue && SelectedEnvironmentVariableIndex.Value >= 0 && InstanceConfig.EnableEnvironment;
+
+
+    public EditInstanceViewModel(EditInstanceWindow parent, string instanceId, InstanceConfig instanceConfig)
     {
         if (Design.IsDesignMode)
         {
             _instanceConfig = new InstanceConfigModel(instanceConfig);
             return;
         }
-        
+
         _parentWindow = parent;
         _instanceId = instanceId;
         _instanceConfig = new InstanceConfigModel(instanceConfig);
         _isInitialized = true;
         SubscribeToConfigChildren(_instanceConfig);
+        if (!string.IsNullOrEmpty(_instanceConfig.Misc.AccountId))
+            _parentWindow.StOverridenAccountInput.SelectedIndex =
+                Accounts.FindIndex(x => x.Id == _instanceConfig.Misc.AccountId);
     }
 
     #region Settings
-    
+
+    /// <summary>
+    /// Subscribes to the PropertyChanged event of the child configuration models
+    /// to monitor changes in their properties.
+    /// </summary>
+    /// <param name="config">The instance configuration model to subscribe to.</param>
     private void SubscribeToConfigChildren(InstanceConfigModel config)
     {
         config.Game.PropertyChanged += OnChildConfigPropertyChanged;
         config.Java.PropertyChanged += OnChildConfigPropertyChanged;
         config.Commands.PropertyChanged += OnChildConfigPropertyChanged;
-        config.Environment.PropertyChanged += OnChildConfigPropertyChanged;
+        config.Environment.CollectionChanged += OnChildConfigCollectionChanged;
         config.Misc.PropertyChanged += OnChildConfigPropertyChanged;
     }
 
+    /// <summary>
+    /// Unsubscribes from the PropertyChanged event of the child configuration models
+    /// to stop monitoring changes in their properties.
+    /// </summary>
+    /// <param name="config">The instance configuration model to unsubscribe from.</param>
     private void UnsubscribeFromConfigChildren(InstanceConfigModel config)
     {
         config.Game.PropertyChanged -= OnChildConfigPropertyChanged;
         config.Java.PropertyChanged -= OnChildConfigPropertyChanged;
         config.Commands.PropertyChanged -= OnChildConfigPropertyChanged;
-        config.Environment.PropertyChanged -= OnChildConfigPropertyChanged;
+        config.Environment.CollectionChanged -= OnChildConfigCollectionChanged;
         config.Misc.PropertyChanged -= OnChildConfigPropertyChanged;
     }
-    
+
+    /// <summary>
+    /// Handles changes to the InstanceConfigModel by unsubscribing from the old configuration
+    /// and subscribing to the new configuration. Saves the new configuration to a file if initialized.
+    /// </summary>
+    /// <param name="oldValue">The previous instance configuration model.</param>
+    /// <param name="newValue">The new instance configuration model.</param>
     partial void OnInstanceConfigChanged(InstanceConfigModel? oldValue, InstanceConfigModel newValue)
     {
         _logger.Debug("InstanceConfig changed with old and new value. Unsubscribing from old, subscribing to new.");
@@ -86,7 +113,12 @@ public partial class EditInstanceViewModel : ObservableObject
         SaveCoreConfigToFile(newValue);
     }
 
-
+    /// <summary>
+    /// Handles the PropertyChanged event for child configuration models.
+    /// Logs the change and saves the updated configuration to a file if initialized.
+    /// </summary>
+    /// <param name="sender">The object that raised the event.</param>
+    /// <param name="e">The event data containing the name of the changed property.</param>
     private void OnChildConfigPropertyChanged(object? sender, PropertyChangedEventArgs e)
     {
         if (!_isInitialized)
@@ -94,8 +126,20 @@ public partial class EditInstanceViewModel : ObservableObject
         _logger.Debug($"Inner property '{e.PropertyName}' changed on {sender?.GetType().Name}. Saving to file...");
         SaveCoreConfigToFile(InstanceConfig);
     }
+    
+    private void OnChildConfigCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+    {
+        if (!_isInitialized)
+            return;
+        _logger.Debug($"Inner collection changed on {sender?.GetType().Name}. Saving to file...");
+        SaveCoreConfigToFile(InstanceConfig);
+    }
 
-
+    /// <summary>
+    /// Saves the updated instance configuration to a file. Ensures that the minimum memory
+    /// does not exceed the maximum memory in the Java configuration.
+    /// </summary>
+    /// <param name="newValue">The updated instance configuration model to save.</param>
     private void SaveCoreConfigToFile(InstanceConfigModel newValue)
     {
         if (newValue.Java.MinMemory > newValue.Java.MaxMemory)
@@ -117,6 +161,10 @@ public partial class EditInstanceViewModel : ObservableObject
 
         if (instanceToSave == null)
             return;
+        
+        var environmentVariables = newValue.Environment
+            .Select(x => new EnvironmentVariable(x.Key, x.Value))
+            .ToList();
 
         instanceToSave.Config = new InstanceConfig()
         {
@@ -147,7 +195,7 @@ public partial class EditInstanceViewModel : ObservableObject
                 PostExitCommand = newValue.Commands.PostExitCommand,
             },
             EnableEnvironment = newValue.EnableEnvironment,
-            Environment = newValue.Environment.ToDictionary(),
+            Environment = environmentVariables,
             Misc = new InstanceMiscConfig()
             {
                 UseCustomGlfw = newValue.Misc.UseCustomGlfw,
@@ -165,5 +213,6 @@ public partial class EditInstanceViewModel : ObservableObject
         JsonHelper.WriteJsonFile(PathHelper.LauncherInstancesPath, instances);
         App.InvokeInstancesChanged();
     }
+
     #endregion
 }

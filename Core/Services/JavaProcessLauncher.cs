09df198f729f4b65952bb38e75204eec282c2346
tavstal <tavstal@gmail.com>
2025-08-11 15:21:45 +0200

    Add support for environment variables in Java process and command execution

--- Core/Services/JavaProcessLauncher.cs
+++ Core/Services/JavaProcessLauncher.cs
@@ -12,20 +12,22 @@ public static class JavaProcessLauncher
 {
     // Logger instance for the JavaProcessLauncher module
     private static readonly CoreLogger _logger = CoreLogger.WithModuleType(typeof(JavaProcessLauncher));
-
+    
     /// <summary>
-    /// Starts a Java process with the specified arguments and an optional wrapper command.
+    /// Starts a Java process with the specified arguments and optional wrapper command and environment variables.
     /// </summary>
-    /// <param name="javaPath">The path to the Java executable. If null or empty, "java" is used as the default.</param>
+    /// <param name="javaPath">The path to the Java executable. If null or empty, "java" will be used as the default.</param>
     /// <param name="arguments">The arguments to pass to the Java process.</param>
     /// <param name="wrapperCommand">
-    /// An optional wrapper command to execute before the Java process. 
-    /// If it contains "%command%", it will be replaced with the Java command; otherwise, it is prepended to the Java command.
+    /// An optional wrapper command to execute the Java process. If it contains "%command%", it will be replaced with the Java path.
+    /// </param>
+    /// <param name="environmentVariables">
+    /// An optional dictionary of environment variables to set for the process.
     /// </param>
     /// <returns>
-    /// A <see cref="Process"/> object representing the started Java process, or <c>null</c> if the process could not be started.
+    /// A <see cref="Process"/> object representing the started Java process, or null if the process could not be started.
     /// </returns>
-    public static Process? StartJava(string javaPath, string arguments, string? wrapperCommand = null)
+    public static Process? StartJava(string javaPath, string arguments, string? wrapperCommand = null, Dictionary<string, string>? environmentVariables = null)
     {
         string finalJavaPath = string.IsNullOrEmpty(javaPath) ? "java" : javaPath;
     
@@ -47,10 +49,16 @@ public static class JavaProcessLauncher
         // Configure the process start information
         var psi = new ProcessStartInfo()
         {
-                UseShellExecute = false,
-                RedirectStandardError = true,
-                RedirectStandardOutput = true,
+            UseShellExecute = false,
+            RedirectStandardError = true,
+            RedirectStandardOutput = true,
         };
+        // Add environment variables if provided
+        if (environmentVariables != null)
+        {
+            foreach (var kvp in environmentVariables)
+                psi.EnvironmentVariables[kvp.Key] = kvp.Value;
+        }
 
         switch (OSHelper.GetOperatingSystem())
         {
@@ -89,18 +97,95 @@ public static class JavaProcessLauncher
             {
                 if (!string.IsNullOrEmpty(e.Data))
                 {
-                    _logger.Debug($"Java Output: {e.Data}");
+                    _logger.Debug($">> {e.Data}");
                 }
             };
             process.ErrorDataReceived += (sender, e) =>
             {
                 if (!string.IsNullOrEmpty(e.Data))
                 {
-                    _logger.Error($"Java Output: {e.Data}");
+                    _logger.Error($"!! {e.Data}");
                 }
             };
             process.Exited += (sender, e) => { _logger.Info($"Java process exited with code: {process.ExitCode}"); };
 
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+#endif
+        }
+
+        // Start the process and return the Process object
+        return process;
+    }
+    
+    /// <summary>
+    /// Starts a process to execute a custom command with optional environment variables.
+    /// </summary>
+    /// <param name="command">The command to execute.</param>
+    /// <param name="environmentVariables">
+    /// An optional dictionary of environment variables to set for the process.
+    /// </param>
+    /// <returns>
+    /// A <see cref="Process"/> object representing the started process, or null if the process could not be started.
+    /// </returns>
+    public static Process? StartCommand(string command, Dictionary<string, string>? environmentVariables = null)
+    {
+        // Configure the process start information
+        var psi = new ProcessStartInfo()
+        {
+            UseShellExecute = false,
+            RedirectStandardError = true,
+            RedirectStandardOutput = true,
+        };
+        // Add environment variables if provided
+        if (environmentVariables != null)
+        {
+            foreach (var kvp in environmentVariables)
+                psi.EnvironmentVariables[kvp.Key] = kvp.Value;
+        }
+
+        switch (OSHelper.GetOperatingSystem())
+        {
+            case EOperatingSystem.Windows:
+            {
+                psi.FileName = "cmd.exe";
+                psi.Arguments = $"/C \"{command}\"";
+                break;
+            }
+            case EOperatingSystem.MacOS:
+            {
+                psi.FileName = "/bin/zsh";
+                psi.Arguments = $"-c \"{command}\"";
+                break;
+            }
+            case EOperatingSystem.Unknown:
+            case EOperatingSystem.Linux:
+            {
+                psi.FileName = "/bin/sh";
+                psi.Arguments = $"-c \"{command}\"";
+                break;
+            }
+        }
+        
+        var process = Process.Start(psi);
+        if (process != null)
+        {
+            process.EnableRaisingEvents = true;
+#if DEBUG
+            process.OutputDataReceived += (sender, e) =>
+            {
+                if (!string.IsNullOrEmpty(e.Data))
+                {
+                    _logger.Debug($"Custom command: {e.Data}");
+                }
+            };
+            process.ErrorDataReceived += (sender, e) =>
+            {
+                if (!string.IsNullOrEmpty(e.Data))
+                {
+                    _logger.Error($"Custom command: {e.Data}");
+                }
+            };
             process.BeginOutputReadLine();
             process.BeginErrorReadLine();
 #endif

7eddc88b1246f9b2d8d7cb95e1795e918bcb32e7
tavstal <tavstal@gmail.com>
2025-08-12 23:22:05 +0200

    Enhance WorldModel with formatted last played time and additional metadata properties

--- Desktop/Models/Instance/WorldModel.cs
+++ Desktop/Models/Instance/WorldModel.cs
@@ -1,21 +1,86 @@
-using System.Threading.Tasks;
+using System;
 using Avalonia.Media.Imaging;
 using CommunityToolkit.Mvvm.ComponentModel;
+using Tavstal.KonkordLauncher.Common.Translation;
 using Tavstal.KonkordLauncher.Core.Helpers;
 
 namespace Tavstal.KonkordLauncher.Desktop.Models.Instance;
 
+/// <summary>
+/// Represents a model for a Minecraft world, including its metadata and utility methods.
+/// </summary>
 public partial class WorldModel : ObservableObject
 {
+    /// <summary>
+    /// The name of the world.
+    /// </summary>
     [ObservableProperty] private string _name;
+
+    /// <summary>
+    /// The game mode of the world (e.g., Survival, Creative).
+    /// </summary>
     [ObservableProperty] private string _gamemode;
-    [ObservableProperty] private string _lastPlayed;
-    [ObservableProperty] [NotifyPropertyChangedFor(nameof(FormattedSize))] private long _size;
+
+    /// <summary>
+    /// The last played timestamp of the world in milliseconds since the Unix epoch.
+    /// </summary>
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(FormattedLastPlayed))]
+    private long _lastPlayed;
+
+    /// <summary>
+    /// The size of the world in bytes.
+    /// </summary>
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(FormattedSize))]
+    private long _size;
+
+    /// <summary>
+    /// The icon representing the world.
+    /// </summary>
     [ObservableProperty] private Bitmap? _icon;
 
+    /// <summary>
+    /// Gets the formatted size of the world as a human-readable string.
+    /// </summary>
     public string FormattedSize => FileSystemHelper.GetFormatedSize(Size);
-    
-    public WorldModel(string name, string gamemode, string lastPlayed, long size, Bitmap? icon)
+
+    /// <summary>
+    /// Gets the formatted last played time of the world as a human-readable string.
+    /// </summary>
+    public string FormattedLastPlayed
+    {
+        get
+        {
+            DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            epoch = epoch.AddMilliseconds(LastPlayed);
+            var timeDiff = DateTime.UtcNow - epoch;
+
+            if (timeDiff.TotalHours < 1)
+                return TranslationManager.Translate("common.time.pass.minute", timeDiff.Minutes);
+
+            if (timeDiff.TotalDays < 1)
+                return TranslationManager.Translate("common.time.pass.hour", $"{timeDiff.Hours}");
+
+            if (timeDiff.TotalDays < 30)
+                return TranslationManager.Translate("common.time.pass.day", $"{timeDiff.Days}");
+
+            if (timeDiff.TotalDays < 365)
+                return TranslationManager.Translate("common.time.pass.month",  $"{timeDiff.TotalDays / 30:F0}");
+
+            return TranslationManager.Translate("common.time.pass.year", $"{timeDiff.TotalDays / 365:F0}");
+        }
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="WorldModel"/> class.
+    /// </summary>
+    /// <param name="name">The name of the world.</param>
+    /// <param name="gamemode">The game mode of the world.</param>
+    /// <param name="lastPlayed">The last played timestamp of the world.</param>
+    /// <param name="size">The size of the world in bytes.</param>
+    /// <param name="icon">The icon representing the world.</param>
+    public WorldModel(string name, string gamemode, long lastPlayed, long size, Bitmap? icon)
     {
         _name = name;
         _gamemode = gamemode;

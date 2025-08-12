9c850f547d86769546cebc79b77ab511883923e1
tavstal <tavstal@gmail.com>
2025-08-12 16:16:12 +0200

    Implement asynchronous launch functionality for Minecraft instances with enhanced error handling and progress reporting

--- Desktop/Models/InstanceModel.cs
+++ Desktop/Models/InstanceModel.cs
@@ -1,18 +1,35 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Avalonia.Controls;
 using Avalonia.Media.Imaging;
 using CommunityToolkit.Mvvm.ComponentModel;
+using Tavstal.KonkordLauncher.Common.Helpers;
+using Tavstal.KonkordLauncher.Common.Models.Config;
 using Tavstal.KonkordLauncher.Common.Models.InstanceConfig;
+using Tavstal.KonkordLauncher.Common.Translation;
 using Tavstal.KonkordLauncher.Core.Enums;
+using Tavstal.KonkordLauncher.Core.Helpers;
+using Tavstal.KonkordLauncher.Core.Instances;
+using Tavstal.KonkordLauncher.Core.Models;
+using Tavstal.KonkordLauncher.Core.Models.Installer;
+using Tavstal.KonkordLauncher.Core.Models.MojangApi.Meta;
 using Tavstal.KonkordLauncher.Desktop.Helpers;
+using Tavstal.KonkordLauncher.Desktop.Models.Enums;
+using Tavstal.KonkordLauncher.Desktop.Views.Dialogs;
 
 namespace Tavstal.KonkordLauncher.Desktop.Models;
 
 /// <summary>
 /// Represents a model for a Minecraft instance, including its properties and behaviors.
 /// </summary>
-public partial class InstanceModel : ObservableObject
+public partial class InstanceModel : ObservableObject, IProgressReporter
 {
+    private readonly CoreLogger _logger = CoreLogger.WithModuleType(typeof(InstanceModel));
+    private Window? _parentWindow;
+    
     /// <summary>
     /// Gets or sets the unique identifier of the instance.
     /// </summary>
@@ -115,16 +132,419 @@ public partial class InstanceModel : ObservableObject
         if (GameProcess == null)
             return;
 
-        GameProcess.Exited += (sender, args) =>
+        GameProcess.Exited += (_, _) =>
         {
             IsGameRunning = false;
             GameProcess = null;
         };
 
-        GameProcess.Disposed += (sender, args) =>
+        GameProcess.Disposed += (_, _) =>
         {
             IsGameRunning = false;
             GameProcess = null;
         };
     }
+    
+    /// <summary>
+    /// Launches the Minecraft instance asynchronously with the specified settings and configurations.
+    /// Handles account selection, environment setup, and game instance initialization.
+    /// </summary>
+    /// <param name="parentWindow">The parent window used for displaying dialogs or alerts.</param>
+    /// <param name="serverAddress">An optional server address to join upon launching the game.</param>
+    public async Task LaunchAsync(Window parentWindow, string? serverAddress = null)
+    {
+        _parentWindow = parentWindow;
+        _logger.Debug($"Launching instance: {Name}");
+        var accountData = await LauncherHelper.GetAccountDataAsync();
+        var account = ConfigModel.Misc.OverrideAccount ? 
+            accountData.Accounts.FirstOrDefault(x => x.Id == ConfigModel.Misc.AccountId) 
+            : accountData.Accounts.FirstOrDefault(x => x.Id == accountData.SelectedAccountId);
+        
+        if (account == null)
+        {
+            _logger.Error("No account selected for launching the ");
+            AlertWindow alertWindow = new AlertWindow(TranslationManager.Translate("account.none.title"), TranslationManager.Translate("account.none.message"), EAlertType.Warning);
+            await alertWindow.ShowDialog(parentWindow);
+            return;
+        }
+
+        try
+        {
+            string wrapperCommand = ConfigModel.Commands.WrapperCommand;
+            // Add gamemoderun if enabled
+            if (ConfigModel.Game.EnableFeralGameMode && !wrapperCommand.Contains("gamemoderun"))
+                wrapperCommand = "gamemoderun " + wrapperCommand;
+
+            // Add mangohud if enabled
+            if (ConfigModel.Game.EnableMangoHud && !wrapperCommand.Contains("mangohud"))
+                wrapperCommand = "mangohud " + wrapperCommand;
+
+            // Attempt to force the use of a dedicated GPU if configured
+            var environmentVariables = ConfigModel.EnableEnvironment
+                ? ConfigModel.Environment
+                : [];
+            var gpuInfo = OSHelper.GetDedicatedGpuType();
+            if (ConfigModel.Game.UseDedicatedGpu && gpuInfo != null)
+            {
+                switch (OSHelper.GetOperatingSystem())
+                {
+                    case EOperatingSystem.Windows:
+                    {
+                        switch (gpuInfo.Value.Item1)
+                        {
+                            case "amd":
+                            {
+                                environmentVariables.Add(new("AMD_POWERXPRESS_REQUEST_HIGH_PERFORMANCE", "1"));
+                                break;
+                            }
+                            case "nvidia":
+                            {
+                                environmentVariables.Add(new("__NV_GPU_USE_DISCRETE_GPU", "1"));
+                                break;
+                            }
+                        }
+                        break;
+                    }
+                    case EOperatingSystem.Linux:
+                    {
+                        switch (gpuInfo.Value.Item1)
+                        {
+                            case "amd":
+                            case "intel":
+                            {
+                                environmentVariables.Add(new("DRI_PRIME", "1"));
+                                break;
+                            }
+                            case "nvidia":
+                            {
+                                environmentVariables.Add(new("__NV_PRIME_RENDER_OFFLOAD", "1"));
+                                environmentVariables.Add(new("__GLX_VENDOR_LIBRARY_NAME", "nvidia"));
+                                break;
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
+
+            var envDic = new Dictionary<string, string>();
+            foreach (var env in environmentVariables)
+                envDic[env.Key] = env.Value;
+
+            // Add custom native libraries if configured
+            List<string> nativeLibraries = [];
+            if (ConfigModel.Misc.UseCustomGlfw && System.IO.File.Exists(ConfigModel.Misc.CustomGlfwPath))
+                nativeLibraries.Add(ConfigModel.Misc.CustomGlfwPath);
+            if (ConfigModel.Misc.UseCustomOpenAL && System.IO.File.Exists(ConfigModel.Misc.CustomOpenALPath))
+                nativeLibraries.Add(ConfigModel.Misc.CustomOpenALPath);
+
+            // Set up the game instance with the provided details
+            MinecraftInstance? gameInstance = null;
+            var settings = await LauncherHelper.GetLauncherSettingsAsync();
+            var gameDetails = new GameDetails(
+                ConfigModel.Java.JavaPath,
+                ConfigModel.Java.MinMemory,
+                ConfigModel.Java.MaxMemory,
+                ConfigModel.Java.JvmArguments,
+                MinecraftVersion,
+                Kind,
+                CustomVersion,
+                GameDirectory,
+                ConfigModel.Commands.PreLaunchCommand,
+                wrapperCommand,
+                ConfigModel.Commands.PostExitCommand,
+                envDic,
+                !string.IsNullOrEmpty(serverAddress) ?
+                    serverAddress 
+                    : ConfigModel.Misc.JoinServerOnLaunch ? ConfigModel.Misc.ServerAddress : null
+            );
+            var launcherDetails = new LauncherDetails("KonkordLauncher", App.Version);
+            var clientDetails = new ClientDetails(
+                account.AccessToken,
+                account.DisplayName,
+                account.Uuid,
+                account.Type !=
+                EAccountType
+                    .MICROSOFT // Might support custom login services in the future, that's why I do not use EAccountType.OFFLINE
+            );
+            var resolution = new Resolution(
+                ConfigModel.Game.StartMaximized
+                    ? (uint)App.ScreenSize.Width
+                    : ConfigModel.Game.WindowWidth,
+                ConfigModel.Game.StartMaximized
+                    ? (uint)App.ScreenSize.Height
+                    : ConfigModel.Game.WindowHeight
+            );
+            switch (Kind)
+            {
+                case EMinecraftKind.VANILLA:
+                {
+                    gameInstance = new MinecraftInstance(
+                        gameDetails,
+                        new PathDetails(
+                            settings.Launcher.AssetsDirectoryPath,
+                            settings.Launcher.CacheDirectoryPath,
+                            settings.Launcher.LibrariesDirectoryPath,
+                            settings.Launcher.VersionsDirectoryPath,
+                            settings.Launcher.GetVanillaManifestPath(),
+                            null,
+                            nativeLibraries
+                        ),
+                        launcherDetails,
+                        clientDetails,
+                        resolution,
+                        this
+                    );
+                    break;
+                }
+                case EMinecraftKind.NEOFORGE:
+                {
+                    gameInstance = new NeoForgeInstance(
+                        gameDetails,
+                        new PathDetails(
+                            settings.Launcher.AssetsDirectoryPath,
+                            settings.Launcher.CacheDirectoryPath,
+                            settings.Launcher.LibrariesDirectoryPath,
+                            settings.Launcher.VersionsDirectoryPath,
+                            settings.Launcher.GetVanillaManifestPath(),
+                            settings.Launcher.GetNeoForgeManifestPath(),
+                            nativeLibraries
+                        ),
+                        launcherDetails,
+                        clientDetails,
+                        resolution,
+                        this
+                    );
+                    break;
+                }
+                case EMinecraftKind.FORGE:
+                {
+                    gameInstance = ForgeInstance.GetForgeInstance(
+                        gameDetails,
+                        new PathDetails(
+                            settings.Launcher.AssetsDirectoryPath,
+                            settings.Launcher.CacheDirectoryPath,
+                            settings.Launcher.LibrariesDirectoryPath,
+                            settings.Launcher.VersionsDirectoryPath,
+                            settings.Launcher.GetVanillaManifestPath(),
+                            settings.Launcher.GetForgeManifestPath(),
+                            nativeLibraries
+                        ),
+                        launcherDetails,
+                        clientDetails,
+                        resolution,
+                        this
+                    );
+                    break;
+                }
+                case EMinecraftKind.FABRIC:
+                {
+                    gameInstance = new FabricInstance(
+                        gameDetails,
+                        new PathDetails(
+                            settings.Launcher.AssetsDirectoryPath,
+                            settings.Launcher.CacheDirectoryPath,
+                            settings.Launcher.LibrariesDirectoryPath,
+                            settings.Launcher.VersionsDirectoryPath,
+                            settings.Launcher.GetVanillaManifestPath(),
+                            settings.Launcher.GetFabricManifestPath(),
+                            nativeLibraries
+                        ),
+                        launcherDetails,
+                        clientDetails,
+                        resolution,
+                        this
+                    );
+                    break;
+                }
+                case EMinecraftKind.QUILT:
+                {
+                    gameInstance = new QuiltInstance(
+                        gameDetails,
+                        new PathDetails(
+                            settings.Launcher.AssetsDirectoryPath,
+                            settings.Launcher.CacheDirectoryPath,
+                            settings.Launcher.LibrariesDirectoryPath,
+                            settings.Launcher.VersionsDirectoryPath,
+                            settings.Launcher.GetVanillaManifestPath(),
+                            settings.Launcher.GetQuiltManifestPath(),
+                            nativeLibraries
+                        ),
+                        launcherDetails,
+                        clientDetails,
+                        resolution,
+                        this
+                    );
+                    break;
+                }
+            }
+
+            if (gameInstance == null)
+                return;
+
+            gameInstance.OnSetupDefaultJava += meta => SetupDefaultJavaPath(gameInstance, meta, settings, parentWindow);
+
+            var process = await gameInstance.Start();
+            if (process == null)
+            {
+                _logger.Error("Failed to launch the  Process is null.");
+                return;
+            }
+
+            GameProcess = process;
+            IsGameRunning = true;
+            AttachProcessEvent();
+            
+            if (settings.Minecraft.CloseLauncherOnGameStart)
+            {
+                parentWindow.Close();
+                return;
+            }
+
+            if (settings.Minecraft.CloseLauncherOnGameExit)
+            {
+                GameProcess.Exited += (sender, args) =>
+                {
+                    parentWindow.Close();
+                };
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.Exc($"Failed to launch the {Name} ");
+            _logger.Error(ex);
+        }
+    }
+    
+    /// <summary>
+    /// Sets up the default Java path for the game instance based on the provided metadata and settings.
+    /// </summary>
+    /// <param name="gameInstance">The game instance to configure.</param>
+    /// <param name="meta">The version metadata for the game instance.</param>
+    /// <param name="settings">The launcher settings.</param>
+    /// <param name="parentWindow">The parent window used for displaying dialogs or alerts.</param>
+    private void SetupDefaultJavaPath(MinecraftInstance gameInstance, VersionMeta? meta, CoreConfig settings, Window parentWindow)
+    {
+        string defaultJavaPath = settings.Java.JavaPath;
+        var instances = LauncherHelper.GetInstances();
+        var instanceIndex = instances.FindIndex(x => x.Id == Id);
+
+        if (meta == null)
+        {
+            UpdateJavaPath(gameInstance, defaultJavaPath, instances, instanceIndex);
+            return;
+        }
+        
+        // Check if the Java version specified in the metadata is available, if not attempt to download it
+        var javaInstallations = JavaHelper.LocateJavaInstallations(settings.Launcher.JavaDirectoryPath);
+        if (javaInstallations.All(x => x.Major != meta.JavaVersionMeta.MajorVersion) && string.IsNullOrEmpty(defaultJavaPath))
+        {
+            // Warning: No Java installation found for the specified version.
+            // Stop the game process if it is running.
+            AlertWindow window = new AlertWindow(
+                TranslationManager.Translate("instance.java.notfound.title", meta.JavaVersionMeta.MajorVersion),
+                TranslationManager.Translate("instance.java.notfound.message", meta.JavaVersionMeta.MajorVersion),
+                EAlertType.Warning
+            );
+            if (IsGameRunning && GameProcess != null)
+                GameProcess.Kill();
+
+            window.ShowDialog(parentWindow);
+            return;
+        }
+
+        foreach (var javaInstallation in javaInstallations)
+        {
+            if (meta.JavaVersionMeta != null && javaInstallation.Major == meta.JavaVersionMeta.MajorVersion)
+            {
+                defaultJavaPath = javaInstallation.Path;
+                break;
+            }
+        }
+        UpdateJavaPath(gameInstance, defaultJavaPath, instances, instanceIndex);
+    }
+    
+    /// <summary>
+    /// Updates the Java path for the game instance and saves the updated configuration.
+    /// </summary>
+    /// <param name="gameInstance">The game instance to update.</param>
+    /// <param name="javaPath">The new Java path to set.</param>
+    /// <param name="instances">The list of instances to update.</param>
+    /// <param name="instanceIndex">The index of the current instance in the list.</param>
+    private void UpdateJavaPath(MinecraftInstance gameInstance, string javaPath, List<Common.Models.Instance> instances, int instanceIndex)
+    {
+        gameInstance.UpdateJavaPath(javaPath);
+
+        if (instanceIndex >= 0)
+        {
+            instances[instanceIndex].Config.Java.JavaPath = javaPath;
+            JsonHelper.WriteJsonFile(PathHelper.LauncherInstancesPath, instances);
+        }
+        App.InvokeInstancesChanged();
+    }
+    
+    #region Progress Reporter
+    private InstallWindow? _instanceInstallWindow;
+    
+    /// <summary>
+    /// Sets the progress value for the installation window. If the window is not open, it will be shown.
+    /// </summary>
+    /// <param name="progress">The progress value to set, typically between 0.0 and 1.0.</param>
+    public void SetProgress(double progress)
+    {
+        if (_instanceInstallWindow == null)
+            Show();
+    
+        _instanceInstallWindow?.SetProgress(progress);
+    }
+
+    /// <summary>
+    /// Sets the status message for the installation window. If the window is not open, it will be shown.
+    /// </summary>
+    /// <param name="status">The status message to display.</param>
+    public void SetStatus(string status)
+    {
+        if (_instanceInstallWindow == null)
+            Show();
+    
+        _instanceInstallWindow?.SetStatus(status);
+    }
+
+    /// <summary>
+    /// Sets a translated status message for the installation window. If the window is not open, it will be shown.
+    /// </summary>
+    /// <param name="statusKey">The translation key for the status message.</param>
+    /// <param name="args">Optional arguments to format the translated message.</param>
+    public void SetStatusTranslated(string statusKey, params object[]? args)
+    {
+        if (_instanceInstallWindow == null)
+            Show();
+    
+        _instanceInstallWindow?.SetStatusTranslated(statusKey, args);
+    }
+
+    /// <summary>
+    /// Displays the installation window as a modal dialog. If the window is already open, this method does nothing.
+    /// </summary>
+    public void Show()
+    {
+        if (_instanceInstallWindow != null || _parentWindow == null)
+            return;
+    
+        _instanceInstallWindow = new InstallWindow();
+        _instanceInstallWindow.ShowDialog(_parentWindow);
+    }
+
+    /// <summary>
+    /// Hides the installation window if it is currently open.
+    /// </summary>
+    public void Hide()
+    {
+        if (_instanceInstallWindow == null)
+            return;
+    
+        _instanceInstallWindow.Close();
+        _instanceInstallWindow = null;
+    }
+    #endregion
 }

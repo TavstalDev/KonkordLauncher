e5a62cc09a3e19d5381bf38acf860485578f9765
tavstal <tavstal@gmail.com>
2025-08-13 16:26:11 +0200

    Enhance EditInstanceViewModel with resource pack management and world metadata updates

--- Desktop/Views/Models/EditInstanceViewModel.cs
+++ Desktop/Views/Models/EditInstanceViewModel.cs
@@ -31,6 +31,7 @@ public partial class EditInstanceViewModel : ObservableObject
     private readonly EditInstanceWindow _parentWindow;
     private readonly CoreLogger _logger = CoreLogger.WithModuleType(typeof(EditInstanceViewModel));
     private readonly InstanceModel _instance;
+    public string? GameDirectory => _instance.GameDirectory;
     public bool IsLinux => OSHelper.GetOperatingSystem() == EOperatingSystem.Linux;
     public bool IsVanilla => _instance.Kind == EMinecraftKind.VANILLA;
     public List<Account> Accounts => LauncherHelper.GetAccountData().Accounts;
@@ -40,10 +41,11 @@ public partial class EditInstanceViewModel : ObservableObject
     
     public ObservableCollection<ResourcePackModel> ResourcePacks { get; set; } = [];
     [ObservableProperty] private ResourcePackModel? _selectedResourcePack;
+    [ObservableProperty] private string? _resourcePackSearchQuery = string.Empty;
     
     public ObservableCollection<ShaderPackModel> ShaderPacks { get; set; } = [];
     [ObservableProperty] private ShaderPackModel? _selectedShaderPack;
-    
+
     public ObservableCollection<WorldModel> Worlds { get; set; } = [];
     [ObservableProperty] private WorldModel? _selectedWorld;
     
@@ -74,6 +76,7 @@ public partial class EditInstanceViewModel : ObservableObject
             _parentWindow.StOverridenAccountInput.SelectedIndex =
                 Accounts.FindIndex(x => x.Id == _instanceConfig.Misc.AccountId);
         
+        RefreshResourcePacks();
         RefreshWorlds();
         RefreshServers();
         RefreshScreenshots();
@@ -81,8 +84,314 @@ public partial class EditInstanceViewModel : ObservableObject
     
     #region Refresh Methods
 
+    #region Mods
+
+    
+
+    #endregion
+    
+    #region Resource Packs
+
+    /// <summary>
+    /// Saves the current state of resource packs by renaming their file extensions
+    /// based on their enabled or disabled status. Enabled resource packs have the
+    /// `.zip` extension, while disabled ones have `.zip.dis`.
+    /// </summary>
+    public void SaveResourcePacks()
+    {
+        _logger.Debug("Saving resource packs...");
+        if (_instance.GameDirectory == null)
+            return;
+
+        string resourcePacksDir = Path.Combine(_instance.GameDirectory, "resourcepacks");
+        if (!Directory.Exists(resourcePacksDir))
+            return;
+
+        foreach (var resourcePack in ResourcePacks)
+        {
+            string? newPath = null;
+            if (resourcePack.IsEnabled && resourcePack.Path.EndsWith(".zip.dis"))
+                newPath = resourcePack.Path.Replace(".dis", "");
+            else if (!resourcePack.IsEnabled && resourcePack.Path.EndsWith(".zip"))
+                newPath = resourcePack.Path.Replace(".zip", ".zip.dis");
+            
+            if (newPath == null)
+                continue;
+
+            if (File.Exists(newPath))
+            {
+                _logger.Warn("Skipping save... Resource pack file already exists: " + newPath);
+                continue;
+            }
+            
+            File.Move(resourcePack.Path, newPath);
+            resourcePack.Path = newPath;
+        }
+    }
+
+    /// <summary>
+    /// Handles changes to the `ResourcePacks` collection by saving the updated state
+    /// of resource packs. This method is triggered whenever the collection is modified.
+    /// </summary>
+    /// <param name="sender">The source of the event, typically the `ResourcePacks` collection.</param>
+    /// <param name="e">The event data containing details about the collection change.</param>
+    public void ResourcePacksOnCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+    {
+        SaveResourcePacks();
+    }
+
+    /// <summary>
+    /// Refreshes the list of resource packs by scanning the game directory for resource pack files.
+    /// Updates the `ResourcePacks` collection with metadata such as name, size, and icon for each resource pack.
+    /// </summary>
+    public void RefreshResourcePacks()
+    {
+        if (_instance.GameDirectory == null)
+            return;
+
+        string resourcePacksDir = Path.Combine(_instance.GameDirectory, "resourcepacks");
+        if (!Directory.Exists(resourcePacksDir))
+            return;
+
+        ResourcePacks.CollectionChanged -= ResourcePacksOnCollectionChanged;
+        
+        ResourcePacks.Clear();
+        var resources = Directory.GetFiles(resourcePacksDir, "*").Where(x => x.EndsWith(".zip") || x.EndsWith(".zip.dis"));
+        foreach (var resource in resources)
+        {
+            // Make sure the name does not include the extension
+            var resourceName = Path.GetFileNameWithoutExtension(resource.Replace(".zip.dis", ".zip"));
+            var size = File.ReadAllBytes(resource).LongLength;
+            using var zipFile = ZipFile.OpenRead(resource);
+            Bitmap? icon = null;
+            
+            var iconEntry = zipFile.Entries.FirstOrDefault(x => x.FullName.EndsWith("pack.png", StringComparison.OrdinalIgnoreCase));
+            if (iconEntry != null)
+            {
+                using var iconStream = iconEntry.Open();
+                using var memoryStream = new MemoryStream();
+                iconStream.CopyTo(memoryStream);
+                iconStream.Close();
+                icon = ImageHelper.Base64ToBitmap(Convert.ToBase64String(memoryStream.ToArray()));
+            }
+            icon ??= ImageHelper.LoadFromResource(new Uri("avares://Desktop/Assets/Images/default_world.png"));
+            
+            // TODO: Handle provider
+            var newResourcePack = new ResourcePackModel(resource.EndsWith(".zip"), resourceName, resource, icon, "unknown", size);
+            ResourcePacks.Add(newResourcePack);
+        }
+        
+        ResourcePacks.CollectionChanged += ResourcePacksOnCollectionChanged;
+    }
+
+    #endregion
+    
+    #region Shaders
+
+    
+
+    #endregion
+    
     #region Worlds
 
+    /// <summary>
+    /// Duplicates a Minecraft world by creating a copy of its directory and updating its metadata.
+    /// Generates a unique name for the duplicated world and updates the "LevelName" tag in the `level.dat` file.
+    /// </summary>
+    /// <param name="world">The world to duplicate.</param>
+    public void DuplicateWorld(WorldModel world)
+    {
+        _logger.Debug("Saving worlds...");
+        if (_instance.GameDirectory == null)
+            return;
+        
+        string worldsDir = Path.Combine(_instance.GameDirectory, "saves");
+        if (!Directory.Exists(worldsDir))
+            return;
+        
+        if (!Directory.Exists(world.Path))
+            return;
+            
+        string? newPath = null;
+        string? newName = null;
+        for (int i = 0; i < 100; i++)
+        {
+            // Generate a new path with a unique name
+            newName = $"{world.Name}({i})";
+            newPath = Path.Combine(worldsDir, newName);
+            if (!Directory.Exists(newPath))
+                break;
+        }
+        if (newPath == null)
+        {
+            _logger.Error("Failed to generate a unique path for the duplicated world.");
+            return;
+        }
+
+        try
+        {
+            FileSystemHelper.MoveDirectory(world.Path, newPath, true, false);
+            
+            string levelDatPath = Path.Combine(newPath, "level.dat");
+            using var inputStream = File.OpenRead(levelDatPath);
+            using var gzip = new GZipStream(inputStream, CompressionMode.Decompress);
+            using var mem = new MemoryStream();
+            gzip.CopyTo(mem);
+            mem.Seek(0, SeekOrigin.Begin);
+            var worldData = NbtConvert.ParseNbtStream(mem);
+            inputStream.Close(); // Close the input stream to avoid file lock issues
+            if (worldData == null)
+            {
+                _logger.Error("Failed to parse level.dat for world: " + world.Name);
+                return;
+            }
+
+            // TryGet should not be used here, since it can't convert the INbtTag to NbtCompoundTag
+            // ReSharper disable once CanSimplifyDictionaryLookupWithTryGetValue
+            if (!worldData.ContainsKey("Data"))
+            {
+                _logger.Error("No 'Data' tag found in level.dat for world: " + world.Name);
+                return;
+            }
+
+            var dataTag = worldData["Data"] as NbtCompoundTag;
+            if (dataTag == null)
+            {
+                _logger.Error("Data tag is not a compound tag in level.dat for world: " + world.Name);
+                return;
+            }
+
+            // TryGet should not be used here, as we want to ensure the tag exists
+            // and it will be remowed and replaced with the new name.
+            // ReSharper disable once CanSimplifyDictionaryRemovingWithSingleCall
+            if (!dataTag.ContainsKey("LevelName"))
+            {
+                _logger.Error("No 'LevelName' tag found in level.dat for world: " + world.Name);
+                return;
+            }
+
+            dataTag.Remove("LevelName"); // dataTag[] uses .Add, so the old one should be removed
+            dataTag["LevelName"] = new NbtStringTag(newName);
+
+            using var outputStream = new NbtWriter().CreateNbtStream(worldData);
+            using var fileOutputStream = File.Create(levelDatPath);
+            outputStream.Seek(0, SeekOrigin.Begin);
+            outputStream.CopyTo(fileOutputStream);
+            fileOutputStream.Close();
+        }
+        catch (Exception ex)
+        {
+            _logger.Exc("Failed to duplicate world.");
+            _logger.Error(ex);
+        }
+    }
+    
+    /// <summary>
+    /// Saves the current list of Minecraft worlds by updating their metadata and renaming their directories
+    /// if necessary. This method ensures that the world's name in the `level.dat` file matches the directory name.
+    /// </summary>
+    public void SaveWorlds()
+    {
+        _logger.Debug("Saving worlds...");
+        if (_instance.GameDirectory == null)
+            return;
+        
+        try
+        {
+            string worldsDir = Path.Combine(_instance.GameDirectory, "saves");
+            if (!Directory.Exists(worldsDir))
+                return;
+            
+            foreach (var world in Worlds)
+            {
+                if (!Directory.Exists(world.Path))
+                    continue;
+                
+                string? oldName = Path.GetDirectoryName(world.Path);
+                // This should be changed if other properties will be changed in the future
+                if (oldName == world.Name)
+                    continue;
+                
+                string newPath = Path.Combine(worldsDir, world.Name);
+                if (Directory.Exists(newPath))
+                {
+                    _logger.Warn("World with the same name already exists, skipping save.");
+                    continue;
+                }
+                
+                string levelDatPath = Path.Combine(world.Path, "level.dat");
+                using var inputStream = File.OpenRead(levelDatPath);
+                using var gzip = new GZipStream(inputStream, CompressionMode.Decompress);
+                using var mem = new MemoryStream();
+                gzip.CopyTo(mem);
+                mem.Seek(0, SeekOrigin.Begin);
+                var worldData = NbtConvert.ParseNbtStream(mem);
+                inputStream.Close(); // Close the input stream to avoid file lock issues
+                if (worldData == null)
+                {
+                    _logger.Error("Failed to parse level.dat for world: " + world.Name);
+                    continue;
+                }
+                
+                // TryGet should not be used here, since it can't convert the INbtTag to NbtCompoundTag
+                // ReSharper disable once CanSimplifyDictionaryLookupWithTryGetValue
+                if (!worldData.ContainsKey("Data"))
+                {
+                    _logger.Error("No 'Data' tag found in level.dat for world: " + world.Name);
+                    continue;
+                }
+                
+                var dataTag = worldData["Data"] as NbtCompoundTag;
+                if (dataTag == null)
+                {
+                    _logger.Error("Data tag is not a compound tag in level.dat for world: " + world.Name);
+                    continue;
+                }
+                
+                // TryGet should not be used here, as we want to ensure the tag exists
+                // and it will be remowed and replaced with the new name.
+                // ReSharper disable once CanSimplifyDictionaryRemovingWithSingleCall
+                if (!dataTag.ContainsKey("LevelName"))
+                {
+                    _logger.Error("No 'LevelName' tag found in level.dat for world: " + world.Name);
+                    continue;
+                }
+                
+                dataTag.Remove("LevelName"); // dataTag[] uses .Add, so the old one should be removed
+                dataTag["LevelName"] = new NbtStringTag(world.Name);
+                
+                using var outputStream = new NbtWriter().CreateNbtStream(worldData);
+                using var fileOutputStream = File.Create(levelDatPath);
+                outputStream.Seek(0, SeekOrigin.Begin);
+                outputStream.CopyTo(fileOutputStream);
+                fileOutputStream.Close();
+                FileSystemHelper.MoveDirectory(world.Path, newPath, true);
+                world.Path = newPath;
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.Exc("Failed to save worlds.");
+            _logger.Error(ex);
+        }
+    }
+    
+    /// <summary>
+    /// Handles changes to the `Worlds` collection by saving the updated list of worlds.
+    /// This method is triggered whenever the `Worlds` collection is modified.
+    /// </summary>
+    /// <param name="sender">The source of the event, typically the `Worlds` collection.</param>
+    /// <param name="e">The event data containing details about the collection change.</param>
+    public void WorldsOnCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+    {
+        SaveWorlds();
+    }
+    
+    /// <summary>
+    /// Refreshes the list of Minecraft worlds by scanning the game directory for saved worlds.
+    /// Updates the Worlds collection with metadata such as name, game mode, last played timestamp, 
+    /// seed, size, and icon for each world.
+    /// </summary>
     public void RefreshWorlds()
     {
         if (_instance.GameDirectory == null)
@@ -91,6 +400,8 @@ public partial class EditInstanceViewModel : ObservableObject
         string worldsDir = Path.Combine(_instance.GameDirectory, "saves");
         if (!Directory.Exists(worldsDir))
             return;
+
+        Worlds.CollectionChanged -= WorldsOnCollectionChanged;
         
         Worlds.Clear();
         var worldDirs = Directory.GetDirectories(worldsDir);
@@ -100,6 +411,7 @@ public partial class EditInstanceViewModel : ObservableObject
             string levelDatPath = Path.Combine(worldDir, "level.dat");
             string gamemode = "unknown";
             long lastPlayed = 0;
+            long seed = 0;
             Bitmap? icon = null;
 
             if (File.Exists(levelDatPath))
@@ -110,6 +422,7 @@ public partial class EditInstanceViewModel : ObservableObject
                 gzip.CopyTo(mem);
                 mem.Seek(0, SeekOrigin.Begin);
                 var data = NbtConvert.DeserializeObject<Level>(mem);
+                inputStream.Close(); // Close the input stream to avoid file lock issues
 
                 if (data != null)
                 {
@@ -117,6 +430,9 @@ public partial class EditInstanceViewModel : ObservableObject
 
                     if (!string.IsNullOrEmpty(data.Data.LevelName))
                         worldName = data.Data.LevelName;
+
+                    if (data.Data.RandomSeed != null)
+                        seed = data.Data.RandomSeed.Value;
                     
                     // GameMode (int) — try "GameType" first, then "GameMode"
                     if (data.Data.GameMode != null)
@@ -155,8 +471,9 @@ public partial class EditInstanceViewModel : ObservableObject
             }
             icon ??= ImageHelper.LoadFromResource(new Uri("avares://Desktop/Assets/Images/default_world.png"));
             
-            Worlds.Add(new WorldModel(worldName, gamemode, lastPlayed, size, icon));
+            Worlds.Add(new WorldModel(worldName, worldDir, gamemode,  seed, lastPlayed, size, icon));
         }
+        Worlds.CollectionChanged += WorldsOnCollectionChanged;
     }
 
     #endregion

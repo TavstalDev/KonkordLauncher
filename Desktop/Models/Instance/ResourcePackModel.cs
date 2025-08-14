a91be40b3a9168f6edb6eeb5db8b6c2d94f27bba
tavstal <tavstal@gmail.com>
2025-08-14 13:06:47 +0200

    Remove unused EnableDisableText property from ResourcePackModel

--- Desktop/Models/Instance/ResourcePackModel.cs
+++ Desktop/Models/Instance/ResourcePackModel.cs
@@ -17,7 +17,7 @@ public partial class ResourcePackModel : ObservableObject
     /// <summary>
     /// Indicates whether the resource pack is enabled.
     /// </summary>
-    [ObservableProperty] [NotifyPropertyChangedFor(nameof(EnableDisableText))] private bool _isEnabled;
+    [ObservableProperty] private bool _isEnabled;
 
     /// <summary>
     /// The name of the resource pack.
@@ -51,14 +51,6 @@ public partial class ResourcePackModel : ObservableObject
     /// Gets the formatted size of the resource pack as a human-readable string.
     /// </summary>
     public string FormattedSize => FileSystemHelper.GetFormatedSize(Size);
-    
-    /// <summary>
-    /// Gets the text to display for enabling or disabling the resource pack.
-    /// Returns a translated string based on the current enabled state of the resource pack.
-    /// </summary>
-    public string EnableDisableText => IsEnabled 
-        ? TranslationManager.Translate("common.disable") 
-        : TranslationManager.Translate("common.enable");
 
     public ResourcePackModel(bool isEnabled, string name, string path, Bitmap? icon, string? provider, long size)
     {

f1561e63fb6535518632bc7f795c945a96c30a47
tavstal <tavstal@gmail.com>
2025-08-15 21:02:29 +0200

    Remove ViewModelBase

--- Desktop/Models/ViewModelBase.cs
+++
@@ -1,8 +0,0 @@
-using ReactiveUI;
-
-namespace Tavstal.KonkordLauncher.Desktop.Models;
-
-/// <summary>
-/// Serves as the base class for all view models in the application, providing reactive property change notifications.
-/// </summary>
-public class ViewModelBase : ReactiveObject;

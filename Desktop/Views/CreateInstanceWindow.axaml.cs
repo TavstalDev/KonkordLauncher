777b33075a16d43acac6ff4ea072fd36d08e7bad
tavstal <tavstal@gmail.com>
2025-08-15 21:04:29 +0200

    Refactor CreateInstanceViewModel and CreateInstanceWindow to enhance command usage, memory management, and improve data binding

--- Desktop/Views/CreateInstanceWindow.axaml.cs
+++ Desktop/Views/CreateInstanceWindow.axaml.cs
@@ -1,17 +1,5 @@
-using System.Linq;
 using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Interactivity;
-using Tavstal.KonkordLauncher.Common.Helpers;
-using Tavstal.KonkordLauncher.Common.Models;
-using Tavstal.KonkordLauncher.Common.Models.Config;
-using Tavstal.KonkordLauncher.Common.Models.InstanceConfig;
-using Tavstal.KonkordLauncher.Common.Translation;
-using Tavstal.KonkordLauncher.Core.Enums;
-using Tavstal.KonkordLauncher.Core.Helpers;
 using Tavstal.KonkordLauncher.Desktop.Models;
-using Tavstal.KonkordLauncher.Desktop.Models.Enums;
-using Tavstal.KonkordLauncher.Desktop.Views.Dialogs;
 using Tavstal.KonkordLauncher.Desktop.Views.Models;
 
 namespace Tavstal.KonkordLauncher.Desktop.Views;
@@ -21,7 +9,7 @@ namespace Tavstal.KonkordLauncher.Desktop.Views;
 /// Initializes the UI components, attaches Avalonia Dev Tools in debug mode,
 /// and sets the DataContext to a new <see cref="CreateInstanceViewModel"/>.
 /// </summary>
-public partial class CreateInstanceWindow : Window
+public partial class CreateInstanceWindow : KonkordWindow
 {
     /// <summary>
     /// Initializes a new instance of the <see cref="CreateInstanceWindow"/> class.
@@ -36,170 +24,11 @@ public partial class CreateInstanceWindow : Window
         this.AttachDevTools();
 #endif
 
-        this.DataContext = new CreateInstanceViewModel();
+        this.DataContext = new CreateInstanceViewModel(this);
     }
 
-    #region Custom - Mod Loader Select
-    /// <summary>
-    /// Handles the click event for the "No Mod Loader" option.
-    /// Sets the ModLoaderType of the ViewModel to VANILLA.
-    /// </summary>
-    /// <param name="sender">The source of the event.</param>
-    /// <param name="e">The event data.</param>
-    private void CustomNoModLoader_Click(object? sender, RoutedEventArgs e)
+    protected override void FreeMemory()
     {
-        if (DataContext is not CreateInstanceViewModel vm) return;
 
-        vm.ModLoaderType = EMinecraftKind.VANILLA;
-    }
-
-    /// <summary>
-    /// Handles the click event for the "NeoForge" mod loader option.
-    /// Sets the ModLoaderType of the ViewModel to NEOFORGE.
-    /// </summary>
-    /// <param name="sender">The source of the event.</param>
-    /// <param name="e">The event data.</param>
-    private void CustomNeoForge_Click(object? sender, RoutedEventArgs e)
-    {
-        if (DataContext is not CreateInstanceViewModel vm) return;
-
-        vm.ModLoaderType = EMinecraftKind.NEOFORGE;
-    }
-
-    /// <summary>
-    /// Handles the click event for the "Forge" mod loader option.
-    /// Sets the ModLoaderType of the ViewModel to FORGE.
-    /// </summary>
-    /// <param name="sender">The source of the event.</param>
-    /// <param name="e">The event data.</param>
-    private void CustomForge_Click(object? sender, RoutedEventArgs e)
-    {
-        if (DataContext is not CreateInstanceViewModel vm) return;
-
-        vm.ModLoaderType = EMinecraftKind.FORGE;
-    }
-
-    /// <summary>
-    /// Handles the click event for the "Fabric" mod loader option.
-    /// Sets the ModLoaderType of the ViewModel to FABRIC.
-    /// </summary>
-    /// <param name="sender">The source of the event.</param>
-    /// <param name="e">The event data.</param>
-    private void CustomFabric_Click(object? sender, RoutedEventArgs e)
-    {
-        if (DataContext is not CreateInstanceViewModel vm) return;
-
-        vm.ModLoaderType = EMinecraftKind.FABRIC;
-    }
-
-    /// <summary>
-    /// Handles the click event for the "Quilt" mod loader option.
-    /// Sets the ModLoaderType of the ViewModel to QUILT.
-    /// </summary>
-    /// <param name="sender">The source of the event.</param>
-    /// <param name="e">The event data.</param>
-    private void CustomQuilt_Click(object? sender, RoutedEventArgs e)
-    {
-        if (DataContext is not CreateInstanceViewModel vm) return;
-
-        vm.ModLoaderType = EMinecraftKind.QUILT;
-    }
-    #endregion
-
-    /// <summary>
-    /// Handles the click event for the icon selector.
-    /// Opens the IconSelectorWindow dialog and sets the selected icon in the ViewModel if one is chosen.
-    /// </summary>
-    /// <param name="sender">The source of the event.</param>
-    /// <param name="e">The event data.</param>
-    private async void IconSelector_Click(object? sender, RoutedEventArgs e)
-    {
-        // TODO: Replace async void
-        if (DataContext is not CreateInstanceViewModel vm)
-            return;
-
-        IconSelectorWindow window = new();
-        var result = await window.ShowDialog<IconDataModel>(this);
-        // ReSharper disable once ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract
-        if (result == null)
-            return;
-        vm.InstanceIcon = result.Image;
-        vm.InstanceIconPath = result.Path;
-    }
-
-    /// <summary>
-    /// Handles the click event for creating a new instance.
-    /// Validates the instance name for duplicates, adds the new instance to the list,
-    /// saves the updated list to a JSON file, and notifies the application of the change.
-    /// </summary>
-    /// <param name="sender">The source of the event.</param>
-    /// <param name="e">The event data.</param>
-    private void CreateInstance_OnClick(object? sender, RoutedEventArgs e)
-    {
-        if (DataContext is not CreateInstanceViewModel vm)
-            return;
-
-        var settings = LauncherHelper.GetLauncherSettings();
-        var instances = LauncherHelper.GetInstances();
-        if (instances.Any(x => x.Name == vm.InstanceName))
-        {
-            AlertWindow alertWindow = new(TranslationManager.Translate("instance.duplicate.title"),
-                TranslationManager.Translate("instance.duplicate.message"),
-                EAlertType.Error);
-            alertWindow.ShowDialog(this);
-            return;
-        }
-        
-        instances.Add(new Instance
-        {
-            Name = vm.InstanceName,
-            Kind = vm.ModLoaderType,
-            Group = "none",
-            MinecraftVersion = vm.SelectedMinecraftVersion?.Id,
-            CustomVersion = vm.SelectedModLoader?.Version ?? string.Empty,
-            IconPath = vm.InstanceIconPath ?? string.Empty,
-            GameDirectory = System.IO.Path.Combine(settings.Launcher.InstancesDirectoryPath, vm.InstanceName),
-            Config = new InstanceConfig
-            {
-                Game = new InstanceGameConfig
-                {
-                    StartMaximized = settings.Minecraft.StartMaximized,
-                    WindowHeight = (uint)(0.45 * App.ScreenSize.Height),
-                    WindowWidth = (uint)(0.40 * App.ScreenSize.Width),
-                    ShowConsoleWhenGameCrashes = true,
-                    ShowConsoleWhileGameRunning = false,
-                    CloseConsoleOnGameExit = false,
-                    EnableFeralGameMode = settings.Misc.EnableFeralGameMode,
-                    EnableMangoHud = settings.Misc.EnableMangoHud,
-                    UseDedicatedGpu = settings.Misc.UseDedicatedGpu 
-                },
-                Java = new JavaConfig
-                {
-                    JvmArguments = string.IsNullOrEmpty(settings.Java.JvmArguments) ? Instance.GetDefaultJVMArgs() : settings.Java.JvmArguments,
-                    JavaPath = "LAUNCH_ME_FIRST",
-                    MinMemory = settings.Java.MinMemory,
-                    MaxMemory = settings.Java.MaxMemory,
-                    PermaGen = settings.Java.PermaGen,
-                },
-                Commands = new InstanceCommandsConfig(),
-                EnableEnvironment = false,
-                Environment = [],
-                Misc =new InstanceMiscConfig()
-            }
-        });
-        JsonHelper.WriteJsonFile(PathHelper.LauncherInstancesPath, instances);
-        App.InvokeInstancesChanged();
-        this.Close();
-    }
-
-    /// <summary>
-    /// Handles the click event for canceling the instance creation process.
-    /// Closes the current window without making any changes.
-    /// </summary>
-    /// <param name="sender">The source of the event.</param>
-    /// <param name="e">The event data.</param>
-    private void CancelInstance_OnClick(object? sender, RoutedEventArgs e)
-    {
-        this.Close();
     }
 }
--- Desktop/Views/Models/CreateInstanceViewModel.cs
+++ Desktop/Views/Models/CreateInstanceViewModel.cs
@@ -1,21 +1,32 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Threading.Tasks;
 using Avalonia.Controls;
 using Avalonia.Media.Imaging;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Tavstal.KonkordLauncher.Common.Helpers;
+using Tavstal.KonkordLauncher.Common.Models;
+using Tavstal.KonkordLauncher.Common.Models.Config;
+using Tavstal.KonkordLauncher.Common.Models.InstanceConfig;
+using Tavstal.KonkordLauncher.Common.Translation;
 using Tavstal.KonkordLauncher.Core.Enums;
 using Tavstal.KonkordLauncher.Core.Helpers;
 using Tavstal.KonkordLauncher.Core.Models.ModLoaders;
 using Tavstal.KonkordLauncher.Core.Models.MojangApi;
 using Tavstal.KonkordLauncher.Desktop.Helpers;
+using Tavstal.KonkordLauncher.Desktop.Models;
+using Tavstal.KonkordLauncher.Desktop.Models.Enums;
 using Tavstal.KonkordLauncher.Desktop.Models.Instance;
+using Tavstal.KonkordLauncher.Desktop.Views.Dialogs;
 
 namespace Tavstal.KonkordLauncher.Desktop.Views.Models;
 
-public partial class CreateInstanceViewModel : ObservableObject
+public partial class CreateInstanceViewModel : KonkordObservableObject
 {
-    private readonly VersionManifest _vanillaManifest;
+    private CreateInstanceWindow? _parentWindow;
+    private VersionManifest? _vanillaManifest;
     private readonly ReverseMarkdown.Converter _converter = new();
 
     #region Custom
@@ -75,6 +86,9 @@ public partial class CreateInstanceViewModel : ObservableObject
     {
         get
         {
+            if (_vanillaManifest == null)
+                return [];
+            
             var versions = _vanillaManifest.Versions.FindAll(x =>
                 (string.IsNullOrEmpty(SearchQuery) || x.Id.StartsWith(SearchQuery)) &&
                 (x.Type != "release" || ShowReleases) &&
@@ -199,13 +213,136 @@ public partial class CreateInstanceViewModel : ObservableObject
     public bool IsSourceFromFile => SelectedImportSourceIndex == 0;
     #endregion
 
-    public CreateInstanceViewModel()
+    public CreateInstanceViewModel(CreateInstanceWindow parentWindow)
     {
-        InstanceIcon = ImageHelper.Load("avares://Desktop/Assets/Icons/dirt.png").Result;
+        _parentWindow = parentWindow;
+        _instanceIcon = ImageHelper.Load("avares://Desktop/Assets/Icons/dirt.png").Result;
         if (Design.IsDesignMode)
             return;
         
         _vanillaManifest = ManifestHelper.GetMinecraftManifest()!;
         _selectedMinecraftVersion = VersionResult.First();
     }
+
+    public override void FreeMemory()
+    {
+        InstanceName = string.Empty;
+        InstanceGroup = string.Empty;
+        InstanceIcon?.Dispose();
+        InstanceIcon = null;
+        InstanceIconPath = null;
+        SearchQuery = string.Empty;
+        SelectedMinecraftVersion = null;
+        _vanillaManifest = null;
+        
+        ModLoaderSearchQuery = string.Empty;
+        SelectedModLoader = null;
+        ModLoaderVersionResult.Clear();
+    }
+    
+    #region Commands
+
+    #region Custom
+    /// <summary>
+    /// Opens a window to select a custom icon for the instance. 
+    /// If an icon is selected, it updates the instance's icon and its path.
+    /// </summary>
+    [RelayCommand]
+    public async Task CustomIconSelectorAsync()
+    {
+        if (_parentWindow == null)
+            return;
+        
+        IconSelectorWindow window = new();
+        var result = await window.ShowDialog<IconDataModel>(_parentWindow);
+        // ReSharper disable once ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract
+        if (result == null)
+            return;
+        InstanceIcon?.Dispose();
+        InstanceIcon = result.Image;
+        InstanceIconPath = result.Path;
+    }
+    
+    /// <summary>
+    /// Sets the mod loader type for the custom instance creation process.
+    /// </summary>
+    /// <param name="modLoaderType">The type of mod loader to set.</param>
+    [RelayCommand]
+    public void CustomModLoaderType(EMinecraftKind modLoaderType)
+    {
+        ModLoaderType = modLoaderType;
+    }
+
+    /// <summary>
+    /// Creates a new custom instance with the specified settings and adds it to the list of instances.
+    /// Displays an error message if an instance with the same name already exists.
+    /// </summary>
+    [RelayCommand]
+    public async Task CustomCreateAsync()
+    {
+        if (_parentWindow == null)
+            return;
+        
+        var settings = await LauncherHelper.GetLauncherSettingsAsync();
+        var instances = await LauncherHelper.GetInstancesAsync();
+        if (instances.Any(x => x.Name == InstanceName))
+        {
+            AlertWindow alertWindow = new(TranslationManager.Translate("instance.duplicate.title"),
+                TranslationManager.Translate("instance.duplicate.message"),
+                EAlertType.Error);
+            await alertWindow.ShowDialog(_parentWindow);
+            return;
+        }
+        
+        instances.Add(new Instance
+        {
+            Name = InstanceName,
+            Kind = ModLoaderType,
+            Group = "none",
+            MinecraftVersion = SelectedMinecraftVersion?.Id,
+            CustomVersion = SelectedModLoader?.Version ?? string.Empty,
+            IconPath = InstanceIconPath ?? string.Empty,
+            GameDirectory = System.IO.Path.Combine(settings.Launcher.InstancesDirectoryPath, InstanceName),
+            Config = new InstanceConfig
+            {
+                Game = new InstanceGameConfig
+                {
+                    StartMaximized = settings.Minecraft.StartMaximized,
+                    WindowHeight = (uint)(0.45 * App.ScreenSize.Height),
+                    WindowWidth = (uint)(0.40 * App.ScreenSize.Width),
+                    ShowConsoleWhenGameCrashes = true,
+                    ShowConsoleWhileGameRunning = false,
+                    CloseConsoleOnGameExit = false,
+                    EnableFeralGameMode = settings.Misc.EnableFeralGameMode,
+                    EnableMangoHud = settings.Misc.EnableMangoHud,
+                    UseDedicatedGpu = settings.Misc.UseDedicatedGpu 
+                },
+                Java = new JavaConfig
+                {
+                    JvmArguments = string.IsNullOrEmpty(settings.Java.JvmArguments) ? Instance.GetDefaultJVMArgs() : settings.Java.JvmArguments,
+                    JavaPath = "LAUNCH_ME_FIRST",
+                    MinMemory = settings.Java.MinMemory,
+                    MaxMemory = settings.Java.MaxMemory,
+                    PermaGen = settings.Java.PermaGen,
+                },
+                Commands = new InstanceCommandsConfig(),
+                EnableEnvironment = false,
+                Environment = [],
+                Misc =new InstanceMiscConfig()
+            }
+        });
+        await JsonHelper.WriteJsonFileAsync(PathHelper.LauncherInstancesPath, instances);
+        App.InvokeInstancesChanged();
+        _parentWindow?.Close();
+    }
+    
+    /// <summary>
+    /// Cancels the custom instance creation process and closes the parent window.
+    /// </summary>
+    [RelayCommand]
+    public void CustomCancelCreate() => _parentWindow?.Close();
+
+    #endregion
+
+    #endregion
 }

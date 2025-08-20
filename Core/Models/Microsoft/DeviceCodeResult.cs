ce8e1078ad5b86110156b759f42c2406688fbd8b
tavstal <tavstal@gmail.com>
2025-08-20 21:26:45 +0200

    Add DeviceCodeResult class: implement model for Microsoft OAuth device code response

---
+++ Core/Models/Microsoft/DeviceCodeResult.cs
@@ -0,0 +1,63 @@
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace Tavstal.KonkordLauncher.Core.Models.Microsoft;
+
+/// <summary>
+/// Represents the result of a device code request for Microsoft OAuth authentication.
+/// Contains information such as the device code, user code, verification URI, expiration time, and polling interval.
+/// </summary>
+public class DeviceCodeResult
+{
+    /// <summary>
+    /// Gets or sets the device code used for authentication.
+    /// </summary>
+    [JsonProperty("device_code"), JsonPropertyName("device_code")]
+    public string DeviceCode { get; set; }
+
+    /// <summary>
+    /// Gets or sets the user code displayed to the user for authentication.
+    /// </summary>
+    [JsonProperty("user_code"), JsonPropertyName("user_code")]
+    public string UserCode { get; set; }
+
+    /// <summary>
+    /// Gets or sets the URI where the user can verify their device code.
+    /// </summary>
+    [JsonProperty("verification_uri"), JsonPropertyName("verification_uri")]
+    public string VerificationUri { get; set; }
+
+    /// <summary>
+    /// Gets or sets the time in seconds until the device code expires.
+    /// </summary>
+    [JsonProperty("expires_in"), JsonPropertyName("expires_in")]
+    public int ExpiresIn { get; set; }
+
+    /// <summary>
+    /// Gets or sets the interval in seconds at which the client should poll for token updates.
+    /// </summary>
+    [JsonProperty("interval"), JsonPropertyName("interval")]
+    public int Interval { get; set; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DeviceCodeResult"/> class.
+    /// </summary>
+    public DeviceCodeResult() {}
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DeviceCodeResult"/> class with the specified properties.
+    /// </summary>
+    /// <param name="deviceCode">The device code used for authentication.</param>
+    /// <param name="userCode">The user code displayed to the user for authentication.</param>
+    /// <param name="verificationUri">The URI where the user can verify their device code.</param>
+    /// <param name="expiresIn">The time in seconds until the device code expires.</param>
+    /// <param name="interval">The interval in seconds at which the client should poll for token updates.</param>
+    public DeviceCodeResult(string deviceCode, string userCode, string verificationUri, int expiresIn, int interval)
+    {
+        DeviceCode = deviceCode;
+        UserCode = userCode;
+        VerificationUri = verificationUri;
+        ExpiresIn = expiresIn;
+        Interval = interval;
+    }
+}

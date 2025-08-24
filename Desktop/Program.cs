1b4fe0396a8baf1a75dfe0e8c53bd0c8c0bdd807
tavstal <tavstal@gmail.com>
2025-08-24 19:20:36 +0200

    Integrate dependency injection and data protection services in the application host

--- Desktop/Program.cs
+++ Desktop/Program.cs
@@ -1,17 +1,32 @@
 ﻿using System;
 using Avalonia;
 using Avalonia.ReactiveUI;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Tavstal.KonkordLauncher.Desktop;
 
+// ReSharper disable once ClassNeverInstantiated.Global
 class Program
 {
+    public static IHost? AppHost { get; private set; }
+    
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        AppHost = Host.CreateDefaultBuilder(args)
+            .ConfigureServices(services =>
+            {
+                // Register your Data Protection services here.
+                services.AddDataProtection();
+            })
+            .Build();
+        
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()

b439ba13d887ba4d6ba90a355c6ae0f8beab3280
tavstal <tavstal@gmail.com>
2025-11-16 18:00:20 +0100

    Fix URI check in Load method to use ToString for proper HTTP detection

--- Desktop/Helpers/ImageHelper.cs
+++ Desktop/Helpers/ImageHelper.cs
@@ -27,7 +27,7 @@ public static class ImageHelper
     /// <returns>A <see cref="Bitmap"/> object if the image is successfully loaded; otherwise, null.</returns>
     public static async Task<Bitmap?> Load(Uri resourceUri)
     {
-        if (resourceUri.PathAndQuery.StartsWith("http"))
+        if (resourceUri.ToString().StartsWith("http"))
             return await LoadFromWeb(resourceUri) ?? null;
         return LoadFromResource(resourceUri);
     }

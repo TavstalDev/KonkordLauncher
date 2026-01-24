daa5cfa791c017a4e3e68373de9f904b5f869d9e
tavstal <tavstal@gmail.com>
2026-01-24 18:31:50 +0100

    Use target-typed new expressions for CompositeDisposable initialization

--- Desktop/Models/Avalonia/KonkordObservableObject.cs
+++ Desktop/Models/Avalonia/KonkordObservableObject.cs
@@ -9,7 +9,7 @@ public abstract class KonkordObservableObject : ObservableObject, IDisposable
     private bool _isDisposed;
 
     // A CompositeDisposable to store all IDisposable resources (e.g., event subscriptions).
-    protected CompositeDisposable Disposables { get; } = new CompositeDisposable();
+    protected CompositeDisposable Disposables { get; } = new();
     
     public void Dispose()
     {

38095be3ee882a564c79a6d995293fff199f578a
tavstal <tavstal@gmail.com>
2026-04-13 22:04:58 +0200

    Remove VersionData property and related constructor parameter from ModdedData class

--- Core/Models/Installer/ModdedData.cs
+++ Core/Models/Installer/ModdedData.cs
@@ -13,11 +13,6 @@ public class ModdedData
     public string? MainClass { get; set; }
 
     /// <summary>
-    /// Gets or sets the version details associated with the modded data.
-    /// </summary>
-    public VersionDetails VersionData { get; set; }
-
-    /// <summary>
     /// Gets or sets the list of libraries required for the modded data.
     /// </summary>
     public List<LibraryMeta> Libraries { get; set; }
@@ -31,12 +26,10 @@ public class ModdedData
     /// Initializes a new instance of the <see cref="ModdedData"/> class with specified main class, version details, and libraries.
     /// </summary>
     /// <param name="mainClass">The main class of the modded data.</param>
-    /// <param name="versionData">The version details associated with the modded data.</param>
     /// <param name="libraries">The list of libraries required for the modded data.</param>
-    public ModdedData(string? mainClass, VersionDetails versionData, List<LibraryMeta> libraries)
+    public ModdedData(string? mainClass, List<LibraryMeta> libraries)
     {
         MainClass = mainClass;
-        VersionData = versionData;
         Libraries = libraries;
     }
 }

ea10eadfb41d0050f34fc5d0702f4bc16ac44011
tavstal <tavstal@gmail.com>
2026-04-13 22:05:31 +0200

    Refactor StartJava method to separate JVM and game arguments, improve command construction, and enhance logging

--- Core/Services/JavaProcessLauncher.cs
+++ Core/Services/JavaProcessLauncher.cs
@@ -1,4 +1,6 @@
 using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
 using Tavstal.KonkordLauncher.Core.Enums;
 using Tavstal.KonkordLauncher.Core.Helpers.Platform;
 using Tavstal.KonkordLauncher.Core.Models;
@@ -13,51 +15,43 @@ public static class JavaProcessLauncher
     // Logger instance for the JavaProcessLauncher module
     private static readonly CoreLogger _logger = CoreLogger.WithModuleType(typeof(JavaProcessLauncher));
     
-    /// <summary>
-    /// Starts a Java process with the specified parameters and logs its output.
-    /// </summary>
-    /// <param name="javaPath">The path to the Java executable. If null or empty, "java" is used by default.</param>
-    /// <param name="arguments">The arguments to pass to the Java process.</param>
-    /// <param name="logFilePath">The path to the log file where the process output will be redirected.</param>
-    /// <param name="wrapperCommand">
-    /// An optional wrapper command to execute the Java process. If it contains "%command%", 
-    /// it will be replaced with the Java executable path and arguments.
-    /// </param>
-    /// <param name="environmentVariables">
-    /// An optional dictionary of environment variables to set for the process.
-    /// </param>
-    /// <returns>
-    /// A <see cref="Process"/> object representing the started Java process, or null if the process could not be started.
-    /// </returns>
-    public static Process? StartJava(string javaPath, string arguments, string? logFilePath = null, string? wrapperCommand = null, Dictionary<string, string>? environmentVariables = null)
+    public static Process? StartJava(string javaPath, string jvmArguments, string gameArguments, string? logsPath = null, string? wrapperCommand = null, Dictionary<string, string>? environmentVariables = null)
     {
         string finalJavaPath = string.IsNullOrEmpty(javaPath) ? "java" : javaPath;
-    
-        // Construct the full command string
-        string fullCommand;
+
+        ProcessStartInfo psi;
         if (!string.IsNullOrEmpty(wrapperCommand))
         {
+            string cmdstr = finalJavaPath + " " + jvmArguments + " " + gameArguments;
             if (wrapperCommand.Contains("%command%"))
-                fullCommand = wrapperCommand.Replace("%command%", finalJavaPath) + " " + arguments;
+                wrapperCommand = wrapperCommand.Replace("%command%", cmdstr);
             else
-                fullCommand = wrapperCommand + (wrapperCommand.EndsWith(" ") ? "" : " ") + finalJavaPath + " " + arguments;
+                wrapperCommand += cmdstr;
+            
+            string[] cmd = wrapperCommand.Split(' ');
+            psi = new ProcessStartInfo
+            {
+                FileName = cmd[0],
+                Arguments = string.Join(' ', cmd.Skip(1)),
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false
+            };
         }
         else
-            fullCommand = finalJavaPath + " " + arguments;
-        
-        fullCommand = fullCommand.Replace("\"", "\\\"");
-        
-        // Configure the process start information
-        var psi = new ProcessStartInfo()
         {
-#if DEBUG
-            UseShellExecute = false,
-            RedirectStandardError = true,
-            RedirectStandardOutput = true,
-#else
-            UseShellExecute = true,
-#endif
-        };
+            psi = new ProcessStartInfo
+            {
+                FileName = finalJavaPath,
+                Arguments = jvmArguments + " " + gameArguments,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false
+            };
+        }
+        
         // Add environment variables if provided
         if (environmentVariables != null)
         {
@@ -65,56 +59,32 @@ public static class JavaProcessLauncher
                 psi.EnvironmentVariables[kvp.Key] = kvp.Value;
         }
         
-        if (!string.IsNullOrEmpty(logFilePath) && File.Exists(logFilePath))
-            File.Delete(logFilePath);
-        
-        switch (OSHelper.GetOperatingSystem())
-        {
-            case EOperatingSystem.Windows:
-            {
-                psi.FileName = finalJavaPath;
-                psi.Arguments = arguments;
-                // TODO: Implement custom logging if specified
-                break;
-            }
-            case EOperatingSystem.MacOS:
-            {
-                psi.FileName = "/bin/zsh";
-                psi.Arguments = string.IsNullOrEmpty(logFilePath) ? 
-                    $@"-c ""{fullCommand}""" 
-                    : 
-                    $@"-c ""{fullCommand} >> ""{logFilePath}"" 2>&1""";
-                break;
-            }
-            case EOperatingSystem.Unknown:
-            case EOperatingSystem.Linux:
-            {
-                psi.FileName = "/bin/sh";
-                psi.Arguments = string.IsNullOrEmpty(logFilePath) ? 
-                    $@"-c ""{fullCommand}""" 
-                    : 
-                    $@"-c ""{fullCommand} >> '{logFilePath}' 2>&1""";
-                break;
-            }
-        }
-        
         // Log the process start details
-        _logger.Debug($"Java Path: {javaPath}");
         _logger.Debug("Starting Java process with arguments:");
+        _logger.Debug("Java: " + finalJavaPath);
         _logger.Debug("FileName: " + psi.FileName);
         _logger.Debug("Arguments: " + psi.Arguments);
-        //_logger.Debug("Log File Path: " + (string.IsNullOrEmpty(logFilePath) ? "No log file specified" : logFilePath));
-        _logger.Debug($"\n# START OF JAVA ARGUMENTS#\n{arguments.Replace(" ", "\n")}\n# END OF JAVA ARGUMENTS#");
         
 
         var process = Process.Start(psi);
         if (process != null)
         {
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
             process.EnableRaisingEvents = true;
             process.Exited += (_, _) =>
             {
                 _logger.Debug($"Java process exited with code: {process.ExitCode}");
             };
+            /*process.OutputDataReceived += (sender, e) => Console.WriteLine("[JAVA-OUT] " + e.Data);
+            process.ErrorDataReceived += (sender, e) => Console.WriteLine("[JAVA-ERR] " + e.Data);
+            var writer = process.StandardInput;
+            string[] gameArgs = gameArguments.Split(' ');
+            writer.WriteLine(gameArgs[0]); // Write the main class or jar file first
+            _logger.Info("Main class: " + gameArgs[0]);
+            writer.WriteLine(Convert.ToBase64String(Encoding.UTF8.GetBytes(gameArgs.Skip(1).Aggregate((a, b) => a + " " + b)))); // Write the rest of the arguments as a single line
+            writer.Flush();
+            writer.Close();*/
         }
 
         // Start the process and return the Process object
@@ -196,5 +166,14 @@ public static class JavaProcessLauncher
 
         // Start the process and return the Process object
         return process;
+    }
+    
+    private static string QuoteForProcess(string s)
+    {
+        if (string.IsNullOrEmpty(s))
+            return "\"\"";
+        if (s.Contains(" ") || s.Contains("\""))
+            return $"\"{s.Replace("\"", "\\\"")}\"";
+        return s;
     }
 }

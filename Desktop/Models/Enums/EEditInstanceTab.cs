48fdc477f37c0bf5facca15414aba38f77115e7c
tavstal <tavstal@gmail.com>
2026-04-27 20:49:08 +0200

    Add enums for EditInstance and InstanceSettings tabs

---
+++ Desktop/Models/Enums/EEditInstanceTab.cs
@@ -0,0 +1,20 @@
+namespace Tavstal.KonkordLauncher.Desktop.Models.Enums;
+
+public enum EEditInstanceTab
+{
+    LOGS = 0,
+    
+    MODS = 1,
+    
+    RESOURCE_PACKS = 2,
+    
+    SHADER_PACKS = 3,
+    
+    WORLDS = 4,
+    
+    SERVERS = 5,
+    
+    SCREENSHOTS = 6,
+    
+    SETTINGS = 7
+}
---
+++ Desktop/Models/Enums/EInstanceSettingsTab.cs
@@ -0,0 +1,14 @@
+namespace Tavstal.KonkordLauncher.Desktop.Models.Enums;
+
+public enum EInstanceSettingsTab
+{
+    JAVA = 0,
+    
+    GAME = 1,
+    
+    CUSTOM_COMMAND = 2,
+    
+    ENVIRONMENT = 3,
+    
+    MISC = 4
+}

96fbc499a6efbb8b0e9477a9cf0de5dfa75153be
tavstal <tavstal@gmail.com>
2026-04-28 22:38:20 +0200

    Add EModLoader enum for mod loader types

---
+++ Desktop/Models/Enums/EModLoader.cs
@@ -0,0 +1,14 @@
+namespace Tavstal.KonkordLauncher.Desktop.Models.Enums;
+
+public enum EModLoader
+{
+    NONE = 0,
+    
+    NEOFORGE = 1,
+    
+    FORGE = 2,
+    
+    FABRIC = 3,
+    
+    QUILT = 4
+}

d390e809d4d57420b512fab61983df8d17d07105
tavstal <tavstal@gmail.com>
2026-04-28 22:38:38 +0200

    Add FromModrinthProjectAsync method to load ModPackModel from Modrinth project

--- Desktop/Models/Instance/ModPackModel.cs
+++ Desktop/Models/Instance/ModPackModel.cs
@@ -1,5 +1,11 @@
+using System;
 using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 using Avalonia.Media.Imaging;
+using Markdig;
+using Modrinth.Models;
+using Tavstal.KonkordLauncher.Common.Helpers;
+using Tavstal.KonkordLauncher.Desktop.Helpers;
 
 namespace Tavstal.KonkordLauncher.Desktop.Models.Instance;
 
@@ -11,4 +17,33 @@ public class ModPackModel
     public string RawPage { get; set; }
     public ObservableCollection<string> Versions { get; set; }
     public ObservableCollection<string> Tags { get; set; }
+    
+    public static async Task<ModPackModel> FromModrinthProjectAsync(SearchResult project)
+    {
+        return await Task.Run(async () => 
+        {
+            var iconTask = project.IconUrl != null 
+                ? ImageHelper.LoadFromWeb(new Uri(project.IconUrl)) 
+                : Task.FromResult<Bitmap?>(null);
+            
+            var dataTask = ModrinthHelper.GetProjectAsync(project.ProjectId);
+
+            await Task.WhenAll(iconTask, dataTask);
+
+            var projectData = await dataTask;
+            string rawPage = projectData != null 
+                ? Markdown.ToHtml(projectData.Body) 
+                : "No body was provided.";
+
+            return new ModPackModel
+            {
+                Name = project.Title ?? "Unknown",
+                Description = project.Description ?? "Unknown description",
+                Icon = await iconTask,
+                RawPage = rawPage,
+                Versions = new ObservableCollection<string>(project.Versions),
+                Tags = new ObservableCollection<string>(project.Categories)
+            };
+        });
+    }
 }

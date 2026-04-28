7196c08aae276d6ee31efba6c72422ba75aa4750
tavstal <tavstal@gmail.com>
2026-04-28 22:38:04 +0200

    Update EInstanceProvider enum values for instance providers

--- Common/Models/EInstanceProvider.cs
+++ Common/Models/EInstanceProvider.cs
@@ -6,22 +6,17 @@ namespace Tavstal.KonkordLauncher.Common.Models;
 public enum EInstanceProvider
 {
     /// <summary>
-    /// The Konkord instance provider.
-    /// </summary>
-    Konkord = 0,
-
-    /// <summary>
     /// The Prism Launcher instance provider.
     /// </summary>
-    PrismLauncher = 1,
+    PrismLauncher = 0,
 
     /// <summary>
     /// The Modrinth instance provider.
     /// </summary>
-    Modrinth = 2,
+    Modrinth = 1,
 
     /// <summary>
     /// The CurseForge instance provider.
     /// </summary>
-    CurseForge = 3
+    CurseForge = 2
 }

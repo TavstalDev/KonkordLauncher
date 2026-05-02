d087a7a769e0771dc3c76c35620a96cf40b21eb1
tavstal <tavstal@gmail.com>
2026-05-02 22:52:00 +0200

    Refactor ModPackModel to use Version objects and improve image loading

--- Desktop/Models/Instance/ModPackModel.cs
+++ Desktop/Models/Instance/ModPackModel.cs
@@ -1,11 +1,10 @@
-using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using Avalonia.Media.Imaging;
 using Markdig;
 using Modrinth.Models;
 using Tavstal.KonkordLauncher.Common.Helpers;
-using Tavstal.KonkordLauncher.Desktop.Helpers;
 
 namespace Tavstal.KonkordLauncher.Desktop.Models.Instance;
 
@@ -15,33 +14,26 @@ public class ModPackModel
     public string Description { get; set; }
     public Bitmap? Icon { get; set; }
     public string RawPage { get; set; }
-    public ObservableCollection<string> Versions { get; set; }
+    public ObservableCollection<Version> Versions { get; set; }
     public ObservableCollection<string> Tags { get; set; }
     
-    public static async Task<ModPackModel> FromModrinthProjectAsync(SearchResult project)
+    public static async Task<ModPackModel> FromModrinthProjectAsync(Project project, List<Version> versions)
     {
         return await Task.Run(async () => 
         {
             var iconTask = project.IconUrl != null 
-                ? ImageHelper.LoadFromWeb(new Uri(project.IconUrl)) 
+                ? MetaCacheHelper.GetImageAsync(project.IconUrl) 
                 : Task.FromResult<Bitmap?>(null);
             
-            var dataTask = ModrinthHelper.GetProjectAsync(project.ProjectId);
-
-            await Task.WhenAll(iconTask, dataTask);
-
-            var projectData = await dataTask;
-            string rawPage = projectData != null 
-                ? Markdown.ToHtml(projectData.Body) 
-                : "No body was provided.";
+            string rawPage = Markdown.ToHtml(project.Body);
 
             return new ModPackModel
             {
-                Name = project.Title ?? "Unknown",
-                Description = project.Description ?? "Unknown description",
+                Name = project.Title,
+                Description = project.Description,
                 Icon = await iconTask,
                 RawPage = rawPage,
-                Versions = new ObservableCollection<string>(projectData?.Versions ?? []),
+                Versions = new ObservableCollection<Version>(versions),
                 Tags = new ObservableCollection<string>(project.Categories)
             };
         });

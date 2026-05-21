2809efb59687d26c179f7c758bac0904751e82f3
tavstal <tavstal@gmail.com>
2026-05-21 22:58:38 +0200

    Add CategoryModel to manage category properties and translation

---
+++ Desktop/Models/Instance/CategoryModel.cs
@@ -0,0 +1,18 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using Tavstal.KonkordLauncher.Common.Translation;
+
+namespace Tavstal.KonkordLauncher.Desktop.Models.Instance;
+
+public partial class CategoryModel : ObservableObject
+{
+    [ObservableProperty]
+    public partial string Name { get; set; } = string.Empty;
+    
+    [ObservableProperty]
+    public partial string TranslationKey { get; set; } = string.Empty;
+    
+    [ObservableProperty]
+    public partial bool IsChecked { get; set; }
+    
+    public string DisplayName => string.IsNullOrEmpty(TranslationKey) ? Name : TranslationManager.Translate(TranslationKey);
+}

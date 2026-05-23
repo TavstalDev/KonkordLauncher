2fddcb46b0882eb9fdd99254b8c0bfed7b9f77d4
tavstal <tavstal@gmail.com>
2026-05-23 22:16:40 +0200

    Add support for custom icon URL in instance import methods

--- Desktop/Views/Models/CreateInstance/CreateInstanceViewModel.Import.cs
+++ Desktop/Views/Models/CreateInstance/CreateInstanceViewModel.Import.cs
@@ -114,7 +114,7 @@ public partial class CreateInstanceViewModel_Import : KonkordObservableObject
             return;
         }
         
-        if (await InstanceHelper.ImportAsync(ImportPath, EInstanceProvider.Modrinth, App.ScreenResolution, null, null, _parent, cancellationToken) != null)
+        if (await InstanceHelper.ImportAsync(ImportPath, EInstanceProvider.Modrinth, App.ScreenResolution, null, null, null, _parent, cancellationToken) != null)
         {
             _parent.CloseReporter();
             var instances = await LauncherHelper.GetInstancesAsync(cancellationToken);
@@ -162,7 +162,7 @@ public partial class CreateInstanceViewModel_Import : KonkordObservableObject
             await HttpHelper.DownloadFileAsync(ImportPath, tempPath, progress, cancellationToken);
             _parent.CloseReporter();
             
-            if (await InstanceHelper.ImportAsync(tempPath, EInstanceProvider.Modrinth, App.ScreenResolution, null, null, _parent,
+            if (await InstanceHelper.ImportAsync(tempPath, EInstanceProvider.Modrinth, App.ScreenResolution, null, null, null, _parent,
                     cancellationToken) != null)
             {
                 _parent.CloseReporter();

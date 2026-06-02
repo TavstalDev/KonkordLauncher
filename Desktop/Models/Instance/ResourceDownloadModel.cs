eb69f34e53467bfa9677563d654839796c4d3417
tavstal <tavstal@gmail.com>
2026-06-02 17:37:17 +0200

    Add ResourceDownloadModel for managing resource download properties

---
+++ Desktop/Models/Instance/ResourceDownloadModel.cs
@@ -0,0 +1,22 @@
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Tavstal.KonkordLauncher.Core.Enums;
+using Tavstal.KonkordLauncher.Desktop.Models.Avalonia;
+
+namespace Tavstal.KonkordLauncher.Desktop.Models.Instance;
+
+public partial class ResourceDownloadModel : KonkordObservableObject
+{
+    public required string Name { get; set; }
+    
+    public required string FileName { get; set; }
+    
+    public required string Version { get; set; }
+    
+    public required string Url { get; set; }
+    
+    public required EPlatformType Platform { get; set; }
+    
+    [ObservableProperty]
+    public partial bool ShouldDownload { get; set; }
+}
